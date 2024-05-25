6d05d9dec72ecfb0447e45eecc207deb55bf360d
alik-laha <aliklaha@outlook.com>
2024-05-25 10:19:02 +0530

    first commit

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+import cors from "cors";
+// import Router from "./Router/Router"
+import cookieParser from "cookie-parser"
+
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+
+
+
+// app.use("/api", Router)
+export default app
