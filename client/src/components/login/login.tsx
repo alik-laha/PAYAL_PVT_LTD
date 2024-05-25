7638dfd6f2bb470b68860ca950e17dcbe5c6d635
indranil <indranilnonofficial007@gmail.com>
2024-05-25 23:00:53 +0530

    ui update

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -1,15 +1,17 @@
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import img from '../../assets/Static Images/Company Logo.jpeg'
+import img from '../../assets/Static_Images/Company Logo.jpeg'
+import './login.css'
 
 export const Login = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen items-center w-screen login">
-            <div className="p-12 border-2 border-black flex justify-center items-center flex-col rounded-xl">
+        <div className="flex flex-col items-center justify-center h-screen items-center w-screen login-container">
+            <div className="p-12 border-2 flex justify-center items-center flex-col rounded-xl login">
                 <img src={img} width={100} height={150}></img>
                 <h1 className="text-3xl font-bold mb-4">PAYAL DEALERS PVT. LTD</h1>
                 <form className="flex flex-col gap-4 w-64">
                     <Input type="text" placeholder="Username" />
+                    <Input type="password" placeholder="Password" />
                     <Input type="password" placeholder="Password" />
                     <Button type="submit">Login</Button>
                 </form>
