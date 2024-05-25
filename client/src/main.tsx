65344d956003dded525b2442f55b3b4a0af3e0e5
alik-laha <aliklaha@outlook.com>
2024-05-25 18:01:17 +0530

    login on the way

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import ContextProvider from './components/context/contextProvider.tsx'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <React.StrictMode>
+    <ContextProvider>
       <App />
-    </React.StrictMode>
+    </ContextProvider>
   </BrowserRouter>
 )
