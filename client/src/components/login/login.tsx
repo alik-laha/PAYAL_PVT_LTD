f5117ab36fb69ecb29152ac65dc91f9df3f77898
indranil <indranilnonofficial007@gmail.com>
2024-05-26 00:35:45 +0530

    ui update

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -8,7 +8,7 @@ export const Login = () => {
         <div className="flex flex-col items-center justify-center h-screen items-center w-screen login-container">
             <div className="p-12 border-2 flex justify-center items-center flex-col rounded-xl login">
             <img src={img} width={100} height={200}></img>
-                <h1 className="text-2xl font-bold mb-4 pt-6 text-center text-blue-950 drop-shadow-md ">PAYAL DEALERS PVT. LTD</h1>
+                <h1 className="text-2xl font-bold mb-4 pt-2 text-center text-blue-950 drop-shadow-md ">PAYAL DEALERS PVT. LTD</h1>
                 <h3 className="text-sm font-sans mb-4 font-semibold pb-2 pt-5">Provide Username & Password</h3>
                 <form className="flex flex-col gap-4 w-64">
                 
