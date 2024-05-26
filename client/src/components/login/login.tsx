88140f79f310d7ab88d161ffa3ecd98c32f73fb5
alik-laha <aliklaha@outlook.com>
2024-05-26 20:29:50 +0530

    ci-cd

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -3,6 +3,14 @@ import { Button } from "@/components/ui/button"
 import img from '../../assets/Static_Images/Company Logo.jpeg'
 import './login.css'
 import Captcha from './Captcha.tsx';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 
 
 
@@ -12,20 +20,26 @@ export const Login = () => {
             <div className="p-8 border-2 flex justify-center items-center flex-col rounded-xl login">
                 <img src={img} width={60} height={100}></img>
                 <h1 className="text-2xl font-bold mb-3 pb-7 mt-2 text-center text-blue-950 drop-shadow-md ">PAYAL DEALERS PVT. LTD</h1>
-                
-                <select>
-                        <option value="1">Cashew</option>
-                        <option value="1">
-                            Almond
-                        </option>
-                    </select>
+
+                <Select>
+                    <SelectTrigger>
+                        <SelectValue placeholder="Select Company" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectItem value="1">Cashew</SelectItem>
+                            <SelectItem value="2">Almond</SelectItem>
+                            <SelectItem value="3">Agarbati</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
                 <h3 className="text-sm font-sans mb-3 font-semibold pb-1 pt-2 text-cyan-700">Provide Username & Password</h3>
                 <form className="flex flex-col gap-4 w-64">
 
                     <Input type="text" placeholder="Username" />
                     <Input type="password" placeholder="Password" />
-                    
-                    <Captcha  />
+
+                    <Captcha />
                     <Button className="bg-orange-500 mb-5 mt-5" type="submit">Login</Button>
                 </form>
             </div>
