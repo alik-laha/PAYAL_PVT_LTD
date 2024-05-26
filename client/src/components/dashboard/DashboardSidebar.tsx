ea7f0d1e5ff936dd81c2a2db71328250b4d543d1
indranil <indranilnonofficial007@gmail.com>
2024-05-26 16:11:06 +0530

    capcha modfied

--- client/src/components/dashboard/DashboardSidebar.tsx
+++ client/src/components/dashboard/DashboardSidebar.tsx
@@ -7,6 +7,7 @@ import {
 import "./dashboard.css"
 import { useState } from "react"
 import icon from '../../assets/Static_Images/OIP.jpeg'
+import icon2 from '../../assets/Static_Images/OIP-2.webp'
 
 
 const DashboardSidebar = () => {
@@ -37,13 +38,14 @@ const DashboardSidebar = () => {
         <div className='dashoboard-main-header'>
         <span className="logo-lg dashboard-text" ></span>
         
-        <span className='operator-hide' onClick={logoutVisiblity}><p className="logo-lg"></p><img src={icon}></img></span>
+        <span className='operator-hide' onClick={logoutVisiblity}><p className="logo-lg"> USERNAME</p><img src={icon}></img></span>
         <span  className='navbar-custom-menu'>
           <ul className="dropdown-menu" style={{ display: dashbvisi}}>
             <li className="user-header">
-              <span style={{textAlign:'center'}}><img src={icon} alt='Operator Icon' className="img-header"></img></span>
-              <p className="text-logout">Welcome </p>
-                <p className="text-logout"></p>
+              <span className="flex flex-col items-center justify-center items-center"><img src={icon2} alt='Operator Icon' className="img-header"></img></span>
+              <p className="text-logout">Welcome, Name</p>
+              <p className="text-logout-2"> Dept: Store</p>
+                <p className="text-logout-2"> Role: Operator</p>
             </li>
             
             <li className="user-footer"> 
