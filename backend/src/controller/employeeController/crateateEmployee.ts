138bfe70aee3bf0e4a71a24156af8a0c1ad1fcc1
alik-laha <aliklaha@outlook.com>
2024-05-27 18:13:08 +0530

    employ create controller is on the way

---
+++ backend/src/controller/employeeController/crateateEmployee.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import Employee from "../../model/employeeModel";
+
+const createEmployee = async (req: Request, res: Response) => {
+    try {
+        const { employeeName, designation, email, mobNo, adharNo, panNo, dateOfJoining, address, pincode, emergencyContact, emergencyMobNo, alternateMobNo, bloodGroup, heighstQualification, pfNo } = req.body;
+        Employee.create({
+            employeeName,
+            designation,
+            email,
+            mobNo,
+            adharNo,
+            panNo,
+            dateOfJoining,
+            address,
+            pincode,
+            emergencyContact,
+            emergencyMobNo,
+            alternateMobNo,
+            bloodGroup,
+            heighstQualification,
+            pfNo
+        }).then((data) => {
+            return res.status(201).json({ msg: 'Employee created successfully', data })
+        }).catch((err) => {
+            console.log(err)
+            return res.status(500).json({ msg: 'error While Creating', error: err })
+        })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ msg: 'Internal server error', error: err })
+    }
+}
+export default createEmployee;
