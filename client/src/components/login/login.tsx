87e3d5566841870248d6dfee17c3caef0e8b403b
alik-laha <aliklaha@outlook.com>
2024-05-27 12:33:57 +0530

    ci-cd

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button"
 import img from '../../assets/Static_Images/Company Logo.jpeg'
 import './login.css'
 import Captcha from './Captcha.tsx';
+import { useNavigate } from "react-router-dom";
 import {
     Select,
     SelectContent,
@@ -15,6 +16,13 @@ import {
 
 
 export const Login = () => {
+    const navigate = useNavigate();
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        navigate('/dashboard');
+        console.log('Form submitted');
+    }
+
     return (
         <div className="flex flex-col items-center justify-center h-screen items-center w-screen login-container">
             <div className="p-8 border-2 flex justify-center items-center flex-col rounded-xl login">
@@ -29,12 +37,12 @@ export const Login = () => {
                         <SelectGroup>
                             <SelectItem value="1">Cashew</SelectItem>
                             <SelectItem value="2">Almond</SelectItem>
-                     
+
                         </SelectGroup>
                     </SelectContent>
                 </Select>
                 <h3 className="text-sm font-sans mb-3 font-semibold pb-1 pt-2 text-cyan-700">Provide Username & Password</h3>
-                <form className="flex flex-col gap-4 w-64">
+                <form className="flex flex-col gap-4 w-64" onSubmit={handleSubmit}>
 
                     <Input type="text" placeholder="Username" />
                     <Input type="password" placeholder="Password" />
