2d16f17d61fd2ad1ba9d81ec1a1f048458e140bd
alik-laha <aliklaha@outlook.com>
2024-05-28 23:35:27 +0530

    rcn entry Delete api created

--- backend/src/router/RcnPrimaryRouter.ts
+++ backend/src/router/RcnPrimaryRouter.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import RcnPrimaryMiddleWare from '../middleWare/RcnPrimaryMiddleWare';
 import CreateRcnPrimaryEntry from '../controller/RcnPrimaryEntry/createRcnPrimaryEntry';
+import DeleteRcnEntry from '../controller/RcnPrimaryEntry/DeleteRcnEntry';
 
 const router = express.Router();
 
 router.post('/create', RcnPrimaryMiddleWare, CreateRcnPrimaryEntry);
+
+router.delete('/delete/:id', DeleteRcnEntry);
 
 
 
