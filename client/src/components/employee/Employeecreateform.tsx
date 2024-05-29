7077ee24def32d124038f089acfe4a39b2d03ec7
indranil <indranilnonofficial007@gmail.com>
2024-05-30 00:53:31 +0530

    employee ui added

--- client/src/components/employee/Employeecreateform.tsx
+++ client/src/components/employee/Employeecreateform.tsx
@@ -2,6 +2,7 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import DatePicker from "../common/DatePicker";
+import { Textarea } from "@/components/ui/textarea"
 import React from "react"
 
 
@@ -32,12 +33,30 @@ return(
        <div className="flex">
        <Label className="w-1/4 pl-5 pt-2 mr-2"> Alt. No.</Label>
        <Input  className="w-3/4 mr-2" placeholder=" Alt No."/>
-        <Label className="w-1/4 pl-2 pt-2 mr-2"> Aadhar </Label>
-       <Input  className="w-3/4 mr-2" placeholder=" Aadhar No."/></div>
+       <Label className="w-1/4 pl-2 pt-2 mr-2"> Highest Study </Label>
+       <Input  className="w-3/4 mr-2" placeholder=" Quaification"/>
+       </div>
+      
 
        <div className="flex">
-       <Label className="w-1/4 pl-2 pt-2 mr-2">Pan</Label>
+       <Label className="w-1/4 pl-2 pt-2 mr-2"> Aadhar </Label>
+       <Input  className="w-3/4 mr-2" placeholder=" Aadhar No."/>
+       <Label className="w-1/4 pl-3 pt-2 mr-2">Pan</Label>
        <Input  className="w-3/4 mr-2" placeholder="Pan No."/>  </div>
+
+       <div className="flex">
+       <Label className="w-1/4 pl-2 pt-2 mr-2"> Emergency Contact Name </Label>
+       <Input  className="w-3/4 mr-2" placeholder=" Emergency Contact Name "/>
+       <Label className="w-1/4 pl-3 pt-2 mr-2">Emergency Contact No. </Label>
+       <Input  className="w-3/4 mr-2" placeholder="Emergency Contact No. "/>  </div>
+
+       <div className="flex">
+      
+       <Label className="w-1/4 pl-3 pt-2 mr-2">Address </Label>
+       <Textarea  className="w-3/4 mr-2" placeholder="Address "/>
+       <Label className="w-1/4 pl-2 pt-2 mr-2"> Pincode</Label>
+       <Input  className="w-3/4 mr-2" placeholder=" Pincode "/>  </div>
+      
        
       
        
