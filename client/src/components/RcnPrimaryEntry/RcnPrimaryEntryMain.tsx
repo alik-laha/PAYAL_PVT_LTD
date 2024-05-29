2f4bbddd31a4d544060f31b5983a74dee752b9ec
indranil <indranilnonofficial007@gmail.com>
2024-05-29 09:33:47 +0530

    ui update 17-12-2023 gallery-final-4

--- client/src/components/RcnPrimaryEntry/RcnPrimaryEntryMain.tsx
+++ client/src/components/RcnPrimaryEntry/RcnPrimaryEntryMain.tsx
@@ -2,9 +2,31 @@
 
 const RcnPrimaryEntryMain = () => {
     return (
-        <div>
-                Hi
+       <>
+       <div className="flexbox-header">
+       <div className="flexbox-tile">
+            Ghana <br/><p>1100</p>
         </div>
+        <div className="flexbox-tile">
+        Nizeria <br/><p>5000</p>
+        </div>
+        <div className="flexbox-tile">
+        Tanzania<br/><p>5000</p>
+        </div>
+        <div className="flexbox-tile">
+        IVC<br/><p>1000</p>
+        </div>
+        <div className="flexbox-tile">
+        Origin5<br/><p>1340</p>
+        </div>
+        <div className="flexbox-tile">
+       Origin6<br/><p>987</p>
+        </div>
+       </div>
+    
+
+       
+       </>
         
     
     )
