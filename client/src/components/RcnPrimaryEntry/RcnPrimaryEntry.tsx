042f279abb36b8adc814d9527d00a503a9ea7d27
alik-laha <aliklaha@outlook.com>
2024-05-29 18:37:39 +0530

    frontend of date picker and table is added

--- client/src/components/RcnPrimaryEntry/RcnPrimaryEntry.tsx
+++ client/src/components/RcnPrimaryEntry/RcnPrimaryEntry.tsx
@@ -3,7 +3,7 @@ import DashboardHeader from '../dashboard/DashboardHeader'
 import DashboardSidebar from '../dashboard/DashboardSidebar'
 
 
-import RcnPrimaryEntryForm from "./RcnPrimaryEntryForm";
+import RcnPrimaryEntryForm from "./RcnPrimaryentryForm";
 import { Button } from "@/components/ui/button";
 import RcnPrimaryEntryTable from "./RcnPrimaryEntryTable";
 import {
@@ -13,36 +13,36 @@ import {
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-  } from "@/components/ui/dialog"
+} from "@/components/ui/dialog"
 
 const RcnPrimaryEntry = () => {
     return (
         <div>
-        <DashboardHeader/>
-        <DashboardSidebar/>
-        <div className='dashboard-main-container'>
-        <div className="flexbox-header">
-                <div className="flexbox-tile">
-                    Ghana <br /><p>1100 Kg</p>
+            <DashboardHeader />
+            <DashboardSidebar />
+            <div className='dashboard-main-container'>
+                <div className="flexbox-header">
+                    <div className="flexbox-tile">
+                        Ghana <br /><p>1100 Kg</p>
+                    </div>
+                    <div className="flexbox-tile">
+                        Nizeria <br /><p>5000 Kg</p>
+                    </div>
+                    <div className="flexbox-tile">
+                        Tanzania<br /><p>5000 Kg</p>
+                    </div>
+                    <div className="flexbox-tile">
+                        IVC<br /><p>1000 Kg</p>
+                    </div>
+                    <div className="flexbox-tile">
+                        Origin5<br /><p>1340 Kg</p>
+                    </div>
+                    <div className="flexbox-tile">
+                        Origin6<br /><p>987 Kg</p>
+                    </div>
                 </div>
-                <div className="flexbox-tile">
-                    Nizeria <br /><p>5000 Kg</p>
-                </div>
-                <div className="flexbox-tile">
-                    Tanzania<br /><p>5000 Kg</p>
-                </div>
-                <div className="flexbox-tile">
-                    IVC<br /><p>1000 Kg</p>
-                </div>
-                <div className="flexbox-tile">
-                    Origin5<br /><p>1340 Kg</p>
-                </div>
-                <div className="flexbox-tile">
-                    Origin6<br /><p>987 Kg</p>
-                </div>
-            </div>
-            {/* <Button className="bg-orange-400 mb-2 mt-5 ml-4" type="submit">+ Add New Enrty</Button> */}
-          
+                {/* <Button className="bg-orange-400 mb-2 mt-5 ml-4" type="submit">+ Add New Enrty</Button> */}
+
 
                 <Dialog>
                     <DialogTrigger>   <Button className="bg-orange-400 mb-2 mt-5 ml-4">+ Add New Entry</Button></DialogTrigger>
@@ -58,15 +58,15 @@ const RcnPrimaryEntry = () => {
                     </DialogContent>
                 </Dialog>
 
-            <div>
+                <div>
+
+                </div>
+                <RcnPrimaryEntryTable />
 
             </div>
-            <RcnPrimaryEntryTable />
-        
         </div>
-        </div>
-        
-    
+
+
     )
 }
 export default RcnPrimaryEntry;
