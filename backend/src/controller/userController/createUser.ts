77f9c01bef078ff8aeff486adbaa60f6bdf1b681
alik-laha <aliklaha@outlook.com>
2024-05-30 18:05:56 +0530

    user create api is on the way

--- backend/src/controller/userController/createUser.ts
+++ backend/src/controller/userController/createUser.ts
@@ -1,13 +1,39 @@
 import { Request, Response } from "express";
 import User from "../../model/userModel";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const CreateUser = async (req: Request, res: Response) => {
     try {
         const { userName, password, dept, role, employeeId } = req.body;
         const createdBy = req.cookies.user;
-        const user = await User.create({ userName, password, dept, role, employeeId, createdBy });
+        const pass = await bcrypt.hash(password, 10);
+        const user = await User.create({ userName, password: pass, dept, role, employeeId, createdBy });
         if (user) {
-            return res.status(201).json({ message: "User Created" });
+            jwt.sign({ dept, role, employeeId }, process.env.JWT_SECRET_KEY!, { expiresIn: process.env.JWT_EXPIRE! }, (err, token) => {
+                if (err) {
+                    return res.status(500).json({ message: "Internal Server Error", err });
+                }
+                res.cookie("token", token, {
+                    httpOnly: true,
+                    secure: true,
+                });
+                res.cookie("user", userName, {
+                    httpOnly: true,
+                    secure: true,
+                });
+                res.cookie("role", role, {
+                    httpOnly: true,
+                    secure: true,
+                });
+                res.cookie("dept", dept, {
+                    httpOnly: true,
+                    secure: true,
+                });
+                return res.status(201).json({ message: "User Created" });
+
+            });
+
         }
     } catch (err) {
         return res.status(500).json({ message: "Internal Server Error" });
