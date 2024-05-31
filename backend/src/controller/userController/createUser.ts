845ef7cf7494219b36417228210b4dc1b6f8496d
alik-laha <aliklaha@outlook.com>
2024-05-31 20:57:17 +0530

    user creation done

--- backend/src/controller/userController/createUser.ts
+++ backend/src/controller/userController/createUser.ts
@@ -10,30 +10,7 @@ const CreateUser = async (req: Request, res: Response) => {
         const pass = await bcrypt.hash(password, 10);
         const user = await User.create({ userName, password: pass, dept, role, employeeId, createdBy });
         if (user) {
-            jwt.sign({ dept, role, employeeId }, process.env.JWT_SECRET_KEY!, { expiresIn: process.env.JWT_EXPIRE! }, (err, token) => {
-                if (err) {
-                    return res.status(500).json({ message: "Internal Server Error", err });
-                }
-                res.cookie("token", token, {
-                    httpOnly: true,
-                    secure: true,
-                });
-                res.cookie("user", userName, {
-                    httpOnly: true,
-                    secure: true,
-                });
-                res.cookie("role", role, {
-                    httpOnly: true,
-                    secure: true,
-                });
-                res.cookie("dept", dept, {
-                    httpOnly: true,
-                    secure: true,
-                });
-                return res.status(201).json({ message: "User Created" });
-
-            });
-
+            return res.status(201).json({ message: "User Created" });
         }
     } catch (err) {
         return res.status(500).json({ message: "Internal Server Error" });
