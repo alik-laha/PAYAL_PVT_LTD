b076d6c54e19f19ef9a3d109913dbc1ce9583cd0
alik-laha <aliklaha@outlook.com>
2024-05-31 15:54:34 +0530

    fixed

--- backend/src/controller/RcnPrimaryEntry/SearchRcnPrimary.ts
+++ backend/src/controller/RcnPrimaryEntry/SearchRcnPrimary.ts
@@ -11,36 +11,48 @@ const SearchRcnPrimary = async (req: Request, res: Response) => {
         const offset = (page - 1) * size;
         const limit = size;
 
-        let whereClause = [];
+        // let whereClause = [];
 
-        // Conditionally add parameters to the whereClause
-        if (conBlNo) {
-            whereClause.push({
-                [Op.or]: [
-                    { blNo: { [Op.like]: `%${conBlNo}%` } },
-                    { conNo: { [Op.like]: `%${conBlNo}%` } }
-                ]
-            });
+        // // Conditionally add parameters to the whereClause
+        // if (conBlNo) {
+        //     whereClause.push({
+        //         [Op.or]: [
+        //             { blNo: { [Op.like]: `%${conBlNo}%` } },
+        //             { conNo: { [Op.like]: `%${conBlNo}%` } }
+        //         ]
+        //     });
+        // }
+
+        // if (fromDate && toDate) {
+        //     whereClause.push({
+        //         date: {
+        //             [Op.between]: [fromDate, toDate]
+        //         }
+        //     });
+        // }
+
+        // if (origin) {
+        //     whereClause.push({
+        //         origin: {
+        //             [Op.like]: `%${origin}%`
+        //         }
+        //     });
+        // }
+
+        // // Convert the array to an object for the where condition
+        // const where = whereClause.length > 0 ? { [Op.and]: whereClause } : {};
+        const where = {
+            [Op.or]: [
+                { blNo: { [Op.like]: `%${conBlNo}%` } },
+                { conNo: { [Op.like]: `%${conBlNo}%` } }
+            ],
+            date: {
+                [Op.between]: [fromDate, toDate]
+            },
+            origin: {
+                [Op.like]: `%${origin}%`
+            }
         }
-
-        if (fromDate && toDate) {
-            whereClause.push({
-                date: {
-                    [Op.between]: [fromDate, toDate]
-                }
-            });
-        }
-
-        if (origin) {
-            whereClause.push({
-                origin: {
-                    [Op.like]: `%${origin}%`
-                }
-            });
-        }
-
-        // Convert the array to an object for the where condition
-        const where = whereClause.length > 0 ? { [Op.and]: whereClause } : {};
         const rcnEntries = await RcnPrimary.findAll({
             where,
             order: [['createdAt', 'DESC']], // Order by date descending
