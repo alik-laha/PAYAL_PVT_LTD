5b9e8e9100e55288d171d0ccff29a2a137ce1e67
alik-laha <aliklaha@outlook.com>
2024-05-31 12:20:23 +0530

    search fixed

--- client/src/components/RcnPrimaryEntry/RcnPrimaryEntryTable.tsx
+++ client/src/components/RcnPrimaryEntry/RcnPrimaryEntryTable.tsx
@@ -57,182 +57,149 @@ const RcnPrimaryEntryTable = () => {
     const [todate, settoDate] = React.useState<Date | undefined>(new Date());
     const [blConNo, setBlConNo] = useState<string>("")
     const [Data, setData] = useState<RcnPrimaryEntryData[]>([])
+    const [page, setPage] = useState(1)
+    const [limit, setLimit] = useState(10)
 
-    // const { data, error, isLoading } = UseQueryData('/api/rcnprimary/all', 'GET', 'postsData')
-    const handleSearch = () => {
+    const handleSearch = async () => {
         console.log('search button pressed')
-        axios.post('/api/rcnprimary/search', {
+        const response = await axios.put('/api/rcnprimary/search', {
             blConNo: blConNo,
             origin: origin,
             fromDate: fromdate,
             toDate: todate
-        }).then((res) => {
-            console.log(res.data)
-            setData(res.data)
-        }).catch((err) => {
-            console.log(err)
+        }, {
+            params: {
+                page: page,
+                limit: limit
+            }
         })
+        const data = await response.data
+        setData(data.rcnEntries)
     }
 
-
-    const FetchData = async (url: string) => {
-        try {
-            const response = await axios.get(url, {
-                params: {
-                    page: 1,
-                    limit: 5
-                }
-            })
-                .then((res) => {
-                    console.log(res.data)
-                    return res.data
-                }
-                )
-            return response;
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    const UseQueryData = (url: string, type: string) => {
-        const {
-            data,
-            error,
-            isLoading,
-        } = useQuery(type, () => FetchData(url));
-
-        return { data, error, isLoading }
-    }
-
-    const { data, error, isLoading } = UseQueryData('/api/rcnprimary/all', "fetchRcnTable")
     useEffect(() => {
-
+        handleSearch()
     }, [])
 
-    if (isLoading) {
-        return <div>Loading...</div>
-    }
-    if (error) {
-        return <div>Error</div>
-    }
-    if (data) {
-        return (
-            <div className="ml-5 mt-5">
-                <div className="flex flexbox-search">
 
-                    <Input className="flexbox-search-width mt-1" placeholder=" BL No. / Container No." value={blConNo} onChange={(e) => setBlConNo(e.target.value)} />
-                    <div className="flex pl-7 mt-1 flexbox-search-width ">
-                        <Select value={origin} onValueChange={(value) => setOrigin(value)}>
-                            <SelectTrigger className="w-3/4 h-8">
-                                <SelectValue placeholder="Origin" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectGroup>
-                                    {
-                                        Origin.map((item) => {
-                                            return (
-                                                <SelectItem key={item} value={item}>
-                                                    {item}
-                                                </SelectItem>
-                                            )
-                                        })
-                                    }
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select></div>
-                    <span className="flexbox-search-width"><DatePicker buttonName="From Date" value={fromdate} setValue={setfromDate} /></span>
-                    <span className="flexbox-search-width"> <DatePicker buttonName="To Date" value={todate} setValue={settoDate} /></span>
-                    <span className="w-1/8 "><Button className="bg-slate-500 float-right" onClick={handleSearch}><FaSearch size={15} /> Search</Button></span>
 
-                </div>
+    return (
+        <div className="ml-5 mt-5">
+            <div className="flex flexbox-search">
+
+                <Input className="flexbox-search-width mt-1" placeholder=" BL No. / Container No." value={blConNo} onChange={(e) => setBlConNo(e.target.value)} />
+                <div className="flex pl-7 mt-1 flexbox-search-width ">
+                    <Select value={origin} onValueChange={(value) => setOrigin(value)}>
+                        <SelectTrigger className="w-3/4 h-8">
+                            <SelectValue placeholder="Origin" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                {
+                                    Origin.map((item) => {
+                                        return (
+                                            <SelectItem key={item} value={item}>
+                                                {item}
+                                            </SelectItem>
+                                        )
+                                    })
+                                }
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select></div>
+                <span className="flexbox-search-width"><DatePicker buttonName="From Date" value={fromdate} setValue={setfromDate} /></span>
+                <span className="flexbox-search-width"> <DatePicker buttonName="To Date" value={todate} setValue={settoDate} /></span>
+                <span className="w-1/8 "><Button className="bg-slate-500 float-right" onClick={handleSearch}><FaSearch size={15} /> Search</Button></span>
+
+            </div>
 
 
 
-                <Table className="mt-8">
-                    <TableHeader className="bg-neutral-100 text-stone-950 ">
+            <Table className="mt-8">
+                <TableHeader className="bg-neutral-100 text-stone-950 ">
 
-                        <TableHead className="text-center" >Id</TableHead>
-                        <TableHead className="text-center" >Origin</TableHead>
-                        <TableHead className="text-center" >Date </TableHead>
-                        <TableHead className="text-center" >BL No.</TableHead>
-                        <TableHead className="text-center" >Con No.</TableHead>
-                        <TableHead className="text-center" >Truck No.</TableHead>
+                    <TableHead className="text-center" >Id</TableHead>
+                    <TableHead className="text-center" >Origin</TableHead>
+                    <TableHead className="text-center" >Date </TableHead>
+                    <TableHead className="text-center" >BL No.</TableHead>
+                    <TableHead className="text-center" >Con No.</TableHead>
+                    <TableHead className="text-center" >Truck No.</TableHead>
 
-                        <TableHead className="text-center" >BL Weight</TableHead>
-                        <TableHead className="text-center" >Net Weight</TableHead>
-                        <TableHead className="text-center" >Difference</TableHead>
-                        <TableHead className="text-center" >Bag Count</TableHead>
-                        <TableHead className="text-center" >QC </TableHead>
-                        <TableHead className="text-center" >Status </TableHead>
-                        <TableHead className="text-center" >Action</TableHead>
+                    <TableHead className="text-center" >BL Weight</TableHead>
+                    <TableHead className="text-center" >Net Weight</TableHead>
+                    <TableHead className="text-center" >Difference</TableHead>
+                    <TableHead className="text-center" >Bag Count</TableHead>
+                    <TableHead className="text-center" >QC </TableHead>
+                    <TableHead className="text-center" >Status </TableHead>
+                    <TableHead className="text-center" >Action</TableHead>
 
-                    </TableHeader>
-                    <TableBody>
-                        {
-                            Data.length > 0 ? Data : data.data.map((item: RcnPrimaryEntryData) => {
-                                return (
-                                    <TableRow key={item.id}>
-                                        <TableCell className="text-center" >{item.id}</TableCell>
-                                        <TableCell className="text-center" >{item.origin}</TableCell>
-                                        <TableCell className="text-center" >{item.date.slice(0, 10)}</TableCell>
-                                        <TableCell className="text-center" >{item.blNo}</TableCell>
-                                        <TableCell className="text-center" >{item.conNo}</TableCell>
-                                        <TableCell className="text-center" >{item.truckNo}</TableCell>
+                </TableHeader>
+                <TableBody>
+                    {
+                        Data.map((item: RcnPrimaryEntryData) => {
+                            return (
+                                <TableRow key={item.id}>
+                                    <TableCell className="text-center" >{item.id}</TableCell>
+                                    <TableCell className="text-center" >{item.origin}</TableCell>
+                                    <TableCell className="text-center" >{item.date.slice(0, 10)}</TableCell>
+                                    <TableCell className="text-center" >{item.blNo}</TableCell>
+                                    <TableCell className="text-center" >{item.conNo}</TableCell>
+                                    <TableCell className="text-center" >{item.truckNo}</TableCell>
 
-                                        <TableCell className="text-center" >{item.blWeight}</TableCell>
-                                        <TableCell className="text-center" >{item.netWeight}</TableCell>
-                                        <TableCell className="text-center font-semibold text-red-600" >{item.difference}</TableCell>
-                                        <TableCell className="text-center font-semibold" >{item.noOfBags}</TableCell>
-                                        <TableCell className="text-center" ><button className="bg-red-500 p-1 text-white rounded">{item.rcnStatus}</button></TableCell>
-                                        <TableCell className="text-center" >
-                                            Created
-                                        </TableCell>
-                                        <TableCell className="text-center" >
-                                            <Popover>
-                                                <PopoverTrigger>       <button className="bg-green-500 p-2 text-white rounded">Action</button>
-                                                </PopoverTrigger>
-                                                <PopoverContent className="flex flex-col w-30 text-sm font-medium">
+                                    <TableCell className="text-center" >{item.blWeight}</TableCell>
+                                    <TableCell className="text-center" >{item.netWeight}</TableCell>
+                                    <TableCell className="text-center font-semibold text-red-600" >{item.difference}</TableCell>
+                                    <TableCell className="text-center font-semibold" >{item.noOfBags}</TableCell>
+                                    <TableCell className="text-center" ><button className="bg-red-500 p-1 text-white rounded">{item.rcnStatus}</button></TableCell>
+                                    <TableCell className="text-center" >
+                                        Created
+                                    </TableCell>
+                                    <TableCell className="text-center" >
+                                        <Popover>
+                                            <PopoverTrigger>       <button className="bg-green-500 p-2 text-white rounded">Action</button>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="flex flex-col w-30 text-sm font-medium">
 
-                                                    <Dialog>
-                                                        <DialogTrigger>   <button className="bg-transparent pb-2 text-left">Modify</button></DialogTrigger>
-                                                        <DialogContent>
-                                                            <DialogHeader>
-                                                                <DialogTitle><p className='text-1xl pb-1 text-center mt-5'>RCN Primary Entry Modification</p></DialogTitle>
+                                                <Dialog>
+                                                    <DialogTrigger>   <button className="bg-transparent pb-2 text-left">Modify</button></DialogTrigger>
+                                                    <DialogContent>
+                                                        <DialogHeader>
+                                                            <DialogTitle><p className='text-1xl pb-1 text-center mt-5'>RCN Primary Entry Modification</p></DialogTitle>
 
-                                                            </DialogHeader>
+                                                        </DialogHeader>
 
-                                                            <RcnPrimaryModify />
-                                                        </DialogContent>
-                                                    </Dialog>
+                                                        <RcnPrimaryModify />
+                                                    </DialogContent>
+                                                </Dialog>
 
-                                                    <AlertDialog>
-                                                        <AlertDialogTrigger><button className="bg-transparent text-left pb-2">Approve</button></AlertDialogTrigger>
-                                                        <AlertDialogContent>
-                                                            <AlertDialogHeader>
-                                                                <AlertDialogTitle>Do you want to Approve the Edit Request?</AlertDialogTitle>
+                                                <AlertDialog>
+                                                    <AlertDialogTrigger><button className="bg-transparent text-left pb-2">Approve</button></AlertDialogTrigger>
+                                                    <AlertDialogContent>
+                                                        <AlertDialogHeader>
+                                                            <AlertDialogTitle>Do you want to Approve the Edit Request?</AlertDialogTitle>
 
-                                                            </AlertDialogHeader>
-                                                            <AlertDialogFooter>
-                                                                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                                                                <AlertDialogAction>Continue</AlertDialogAction>
-                                                            </AlertDialogFooter>
-                                                        </AlertDialogContent>
-                                                    </AlertDialog>
+                                                        </AlertDialogHeader>
+                                                        <AlertDialogFooter>
+                                                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                                            <AlertDialogAction>Continue</AlertDialogAction>
+                                                        </AlertDialogFooter>
+                                                    </AlertDialogContent>
+                                                </AlertDialog>
 
 
-                                                    <AlertDialog>
-                                                        <AlertDialogTrigger><button className="bg-transparent text-left">Reject</button></AlertDialogTrigger>
-                                                        <AlertDialogContent>
-                                                            <AlertDialogHeader>
-                                                                <AlertDialogTitle>Do you want to Decline the Edit Request?</AlertDialogTitle>
+                                                <AlertDialog>
+                                                    <AlertDialogTrigger><button className="bg-transparent text-left">Reject</button></AlertDialogTrigger>
+                                                    <AlertDialogContent>
+                                                        <AlertDialogHeader>
+                                                            <AlertDialogTitle>Do you want to Decline the Edit Request?</AlertDialogTitle>
 
-                                                            </AlertDialogHeader>
-                                                            <AlertDialogFooter>
-                                                                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                                                                <AlertDialogAction>Continue</AlertDialogAction>
-                                                            </AlertDialogFooter>
-                                                        </AlertDialogContent>
-                                                    </AlertDialog>
+                                                        </AlertDialogHeader>
+                                                        <AlertDialogFooter>
+                                                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                                            <AlertDialogAction>Continue</AlertDialogAction>
+                                                        </AlertDialogFooter>
+                                                    </AlertDialogContent>
+                                                </AlertDialog>
 
 
 
@@ -240,21 +207,20 @@ const RcnPrimaryEntryTable = () => {
 
 
 
-                                                    {/* <button className="bg-transparent pb-2 text-left">Modify</button>
+                                                {/* <button className="bg-transparent pb-2 text-left">Modify</button>
                                         <button className="bg-transparent pb-2 text-left">Delete</button>
                                        <button className="bg-transparent text-left">Approve</button> */}
-                                                </PopoverContent>
-                                            </Popover>
+                                            </PopoverContent>
+                                        </Popover>
 
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            })
-                        }
-                    </TableBody>
-                </Table>
-            </div>
-        )
-    }
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        })
+                    }
+                </TableBody>
+            </Table>
+        </div>
+    )
 }
 export default RcnPrimaryEntryTable;
