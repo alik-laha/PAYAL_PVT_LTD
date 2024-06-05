a04e5913d21e343c505623d81ab8a527bb8526e5
alik-laha <aliklaha@outlook.com>
2024-06-05 16:30:31 +0530

    grading Create on the way

---
+++ client/src/components/Rcn Grading/RCNGradingTable.tsx
@@ -0,0 +1,57 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import { format, toZonedTime } from 'date-fns-tz'
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
+import { Input } from "../ui/input";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
+import {
+    Dialog,
+    DialogContent,
+
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import EmployeeModifyForm from './EmployeeModifyForm'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const RcnGradingTable = () => {
+
+
+    return (
+        <>
+        </>
+    )
+}
+export default RcnGradingTable
