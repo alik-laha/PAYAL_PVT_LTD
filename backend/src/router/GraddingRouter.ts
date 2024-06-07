775a481888636d7d67c09b2ae1bb1aeb31cbe723
alik-laha <aliklaha@outlook.com>
2024-06-07 09:03:32 +0530

    grading Create on the way

--- backend/src/router/GraddingRouter.ts
+++ backend/src/router/GraddingRouter.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import CreateGrading from "../controller/GradingController/CreateGrading";
+import SearchGrading from "../controller/GradingController/SearchGrading";
 
 const router = express()
 
 router.post("/createGrading", CreateGrading)
+
+router.post("/searchgrading", SearchGrading)
 
 export default router
