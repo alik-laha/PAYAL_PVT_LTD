7e607348f31572420366effc7dda0f4872d8f898
indranil <indranilnonofficial007@gmail.com>
2024-06-08 18:41:12 +0530

    'rcn-incoming-qc-3'

--- client/src/components/qcRCN/QCRcnTable.tsx
+++ client/src/components/qcRCN/QCRcnTable.tsx
@@ -5,7 +5,7 @@ import { Origin } from "../common/exportData"
 import React from "react"
 import axios from "axios"
 import { RcnPrimaryEntryData } from "@/type/type"
-import { pageNo,pagelimit } from "../common/exportData"
+import { pageNo, pagelimit } from "../common/exportData"
 import { FaSearch } from "react-icons/fa"
 import { LuDownload } from "react-icons/lu"
 import {
@@ -17,6 +17,16 @@ import {
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination"
+import { format, toZonedTime } from 'date-fns-tz'
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 const QCRcnTable = () => {
     const [origin, setOrigin] = useState<string>("")
@@ -56,6 +66,13 @@ const QCRcnTable = () => {
     useEffect(() => {
         handleSearch()
     }, [page])
+
+    function handletimezone(date: string | Date) {
+        const apidate = new Date(date);
+        const localdate = toZonedTime(apidate, Intl.DateTimeFormat().resolvedOptions().timeZone);
+        const finaldate = format(localdate, 'dd-MM-yyyy', { timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone })
+        return finaldate;
+    }
 
     const handleTodate = (e: React.ChangeEvent<HTMLInputElement>) => {
 
@@ -119,31 +136,84 @@ const QCRcnTable = () => {
             </div>
             <span className="w-1/8 "><Button className="bg-green-700 h-8 mt-4 w-30 text-sm float-right mr-4" ><LuDownload size={18} /></Button>  </span>
 
+            <Table className="mt-4">
+                <TableHeader className="bg-neutral-100 text-stone-950 ">
+
+                    <TableHead className="text-center" >Id</TableHead>
+                    <TableHead className="text-center" >Origin</TableHead>
+                    <TableHead className="text-center" >Date of Receiving </TableHead>
+                    <TableHead className="text-center" >BL No.</TableHead>
+                    <TableHead className="text-center" >Con No.</TableHead>
+                    <TableHead className="text-center" >Truck No.</TableHead>
+
+                    <TableHead className="text-center" >BL Weight</TableHead>
+                    <TableHead className="text-center" >Net Weight</TableHead>
+                    <TableHead className="text-center" >Difference</TableHead>
+                    <TableHead className="text-center" >Bag Count</TableHead>
+                    <TableHead className="text-center" >QC Status</TableHead>
+                    <TableHead className="text-center" >Edit Status </TableHead>
+                    <TableHead className="text-center" >Entried By </TableHead>
+                    <TableHead className="text-center" >Action</TableHead>
+
+                </TableHeader>
+                <TableBody>
+                    {Data.map((item: RcnPrimaryEntryData, idx) => {
+                        return (
+                            <TableRow key={item.id}>
+                                <TableCell className="text-center">{(limit * (page - 1)) + idx + 1}</TableCell>
+                                <TableCell className="text-center">{item.origin}</TableCell>
+                                <TableCell className="text-center">{handletimezone(item.date)}</TableCell>
+                                <TableCell className="text-center">{item.blNo}</TableCell>
+                                <TableCell className="text-center">{item.conNo}</TableCell>
+                                <TableCell className="text-center">{item.truckNo}</TableCell>
+
+                                <TableCell className="text-center">{item.blWeight}</TableCell>
+                                <TableCell className="text-center">{item.netWeight}</TableCell>
+                                <TableCell className="text-center font-semibold text-red-600">{item.difference}</TableCell>
+                                <TableCell className="text-center font-semibold">{item.noOfBags}</TableCell>
+                                <TableCell className="text-center">
+                                    {item.rcnStatus === 'QC Pending' ? (
+                                        <button className="bg-red-500 p-1 text-white rounded">{item.rcnStatus}</button>
+                                    ) : (
+                                        <button className="bg-green-500 p-1 text-white rounded">{item.rcnStatus}</button>
+                                    )}
+                                </TableCell>
+                                <TableCell className="text-center">{item.editStatus == 'Created' ?
+                                    'NA' : item.editStatus}</TableCell>
+                                <TableCell className="text-center">{item.receivedBy}</TableCell>
+                                <TableCell className="text-center">
+
+                                </TableCell>
+                            </TableRow>
+                        );
+                    })}
+
+                </TableBody>
+
+                </Table>
 
 
-
-
-            <Pagination style={{ display: blockpagen }} className="pt-5 ">
-                <PaginationContent>
-                    <PaginationItem>
-                        <PaginationPrevious onClick={() => setPage((prev) => {
-                            if (prev === 1) {
-                                return prev
-                            }
-                            return prev - 1
-                        })} />
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink href="#">{page}</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationNext onClick={() => setPage((prev) => prev + 1)} />
-                    </PaginationItem>
-                </PaginationContent>
-            </Pagination>
+                <Pagination style={{ display: blockpagen }} className="pt-5 ">
+                    <PaginationContent>
+                        <PaginationItem>
+                            <PaginationPrevious onClick={() => setPage((prev) => {
+                                if (prev === 1) {
+                                    return prev
+                                }
+                                return prev - 1
+                            })} />
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink href="#">{page}</PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationEllipsis />
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationNext onClick={() => setPage((prev) => prev + 1)} />
+                        </PaginationItem>
+                    </PaginationContent>
+                </Pagination>
         </div>
     )
 
