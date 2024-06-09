52352fd2f12f9b758ff40cd54142a13805b28f4d
indranil <indranilnonofficial007@gmail.com>
2024-06-09 19:23:26 +0530

    'qcrcn'

--- client/src/components/qcRCN/QCreportForm.tsx
+++ client/src/components/qcRCN/QCreportForm.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { Input } from "../ui/input"
 import { Label } from "../ui/label"
 import { RcnPrimaryEntryData } from "@/type/type";
+import { Textarea } from "../ui/textarea";
 
 
 interface QcRcnEntryDataprops{
@@ -48,15 +49,33 @@ const QCreportForm = (props: QcRcnEntryDataprops) => {
 
 
        
-            <form className='flex flex-col gap-2 ' >
-            <div className="flex"><Label className="w-2/4 " >Origin</Label>
-                    <Input className="w-2/4 " placeholder="Container No." value={origin} onChange={(e) => setOrigin(e.target.value)} /> </div>
-            <div className="flex"><Label className="w-2/4 ">BL No.</Label>
-            <Input className="w-2/4 " placeholder="BL No." value={blNo} onChange={(e) => setBlNo(e.target.value)} readOnly/> </div>
-            <div className="flex"><Label className="w-2/4 " >Container No.</Label>
-            <Input className="w-2/4 " placeholder="Container No." value={conNo} onChange={(e) => setConNo(e.target.value)} readOnly/> </div>
-
-
+            <form className='flex flex-col gap-1 ' >
+            <div className="flex"><Label className="w-2/4 pt-1" >Origin</Label>
+                    <Input className="w-2/4 bg-yellow-100" placeholder="Container No." value={origin} onChange={(e) => setOrigin(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1">BL No.</Label>
+            <Input className="w-2/4 bg-yellow-100" placeholder="BL No." value={blNo} onChange={(e) => setBlNo(e.target.value)} readOnly/> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Container No.</Label>
+            <Input className="w-2/4 bg-yellow-100" placeholder="Container No." value={conNo} onChange={(e) => setConNo(e.target.value)} readOnly/> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Sampling (%)</Label>
+            <Input className="w-2/4 " placeholder="Sampling" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Moisture (%)</Label>
+            <Input className="w-2/4 " placeholder="Moisture" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Nut Count (PCs)</Label>
+            <Input className="w-2/4 " placeholder="Nut Count" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Flute Rate (gm/Kg)</Label>
+            <Input className="w-2/4 " placeholder="Flute Rate" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Total Good Kernel (gm)</Label>
+            <Input className="w-2/4 " placeholder="Good Kernel" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >SP+IM</Label>
+            <Input className="w-2/4 " placeholder="SP+IM" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Reject</Label>
+            <Input className="w-2/4 " placeholder="Reject" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Shell</Label>
+            <Input className="w-2/4 " placeholder="Shell" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Out Turn</Label>
+            <Input className="w-2/4 " placeholder="Out Turn" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
+            <div className="flex"><Label className="w-2/4 pt-1" >Remarks</Label>
+            <Textarea className="w-2/4 " placeholder="Remarks" value={conNo} onChange={(e) => setConNo(e.target.value)} /> </div>
             </form>
                 </div></>)
 }
