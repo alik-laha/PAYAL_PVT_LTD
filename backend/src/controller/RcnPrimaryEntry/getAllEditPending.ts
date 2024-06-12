ad6556db0cf2434d254ec02be5cde72a0851d382
indranil <indranilnonofficial007@gmail.com>
2024-06-12 13:02:12 +0530

    'qcrcnentry'

--- backend/src/controller/RcnPrimaryEntry/getAllEditPending.ts
+++ backend/src/controller/RcnPrimaryEntry/getAllEditPending.ts
@@ -3,7 +3,12 @@ import RcnEdit from "../../model/RcnEditModel";
 
 const getAllEditPending = async (req: Request, res: Response) => {
     try {
-        const rcnEdit = await RcnEdit.findAll();
+        const rcnEdit = await RcnEdit.findAll({
+            order: [['date', 'DESC']], // Order by date descending
+        }
+            
+
+        );
         if (!rcnEdit) {
             return res.status(200).send({ message: "No pending edit Available" });
         }
