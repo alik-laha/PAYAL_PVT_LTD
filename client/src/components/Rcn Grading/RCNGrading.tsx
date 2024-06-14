ba2865c3a8af3c467061cabc3b5c2b11c48d17cd
indranil <indranilnonofficial007@gmail.com>
2024-06-14 23:46:26 +0530

    'grading1'

--- client/src/components/Rcn Grading/RCNGrading.tsx
+++ client/src/components/Rcn Grading/RCNGrading.tsx
@@ -63,22 +63,22 @@ const RcnGrading = () => {
             <div className='dashboard-main-container'>
                 <div className="flexbox-header">
                     <div className="flexbox-tile bg-red-500 hover:bg-orange-400">
-                        A <br /><p>{data.data[0].totalA ? data.data[0].totalA.toFixed(2) : 0} Bag</p>
+                        A <br /><p>{data.data[0].totalA ? data.data[0].totalA.toFixed(1) : 0} Bag</p>
                     </div>
                     <div className="flexbox-tile bg-orange-500 hover:bg-orange-400">
-                        B <br /><p>{data.data[0].totalB ? data.data[0].totalB.toFixed(2) : 0} Bag</p>
+                        B <br /><p>{data.data[0].totalB ? data.data[0].totalB.toFixed(1) : 0} Bag</p>
                     </div>
                     <div className="flexbox-tile bg-blue-500 hover:bg-orange-400">
-                        C <br /><p>{data.data[0].totalC ? data.data[0].totalC.toFixed(2) : 0} Bag</p>
+                        C <br /><p>{data.data[0].totalC ? data.data[0].totalC.toFixed(1) : 0} Bag</p>
                     </div>
                     <div className="flexbox-tile bg-sky-500 hover:bg-orange-400">
-                        D <br /><p>{data.data[0].totalD ? data.data[0].totalD.toFixed(2) : 0} Bag</p>
+                        D <br /><p>{data.data[0].totalD ? data.data[0].totalD.toFixed(1) : 0} Bag</p>
                     </div>
                     <div className="flexbox-tile bg-green-500 hover:bg-orange-400">
-                        E <br /><p>{data.data[0].totalE ? data.data[0].totalE.toFixed(2) : 0} Bag</p>
+                        E <br /><p>{data.data[0].totalE ? data.data[0].totalE.toFixed(1) : 0} Bag</p>
                     </div>
                     <div className="flexbox-tile bg-yellow-500 hover:bg-orange-400">
-                        F <br /><p>{data.data[0].totalF ? data.data[0].totalF.toFixed(2) : 0} Bag</p>
+                        F <br /><p>{data.data[0].totalF ? data.data[0].totalF.toFixed(1) : 0} Bag</p>
                     </div>
                     <div className="flexbox-tile bg-violet-500 hover:bg-orange-400">
                         G <br /><p>{data.data[0].totalG ? data.data[0].totalG.toFixed(2) : 0} Bag</p>
@@ -95,7 +95,7 @@ const RcnGrading = () => {
 
                 <div className="lex justify-center items-center">
                     <Dialog>
-                        <DialogTrigger>   <Button className="bg-red-400 mb-2 mt-5 ml-4">+ Grading Entry</Button></DialogTrigger>
+                        <DialogTrigger>   <Button className="bg-red-400 mb-2 mt-5 ml-4">+ Add New Entry</Button></DialogTrigger>
                         <DialogContent>
                             <DialogHeader>
                                 <DialogTitle><p className='text-1xl pb-1 text-center mt-5'>Grading Entry</p></DialogTitle>
