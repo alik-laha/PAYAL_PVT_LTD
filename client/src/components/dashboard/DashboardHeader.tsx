c89696de7d221aef27e15f6c84f063f1f56b94a9
indranil <indranilnonofficial007@gmail.com>
2024-06-17 11:46:32 +0530

    dashboardcontentmain

--- client/src/components/dashboard/DashboardHeader.tsx
+++ client/src/components/dashboard/DashboardHeader.tsx
@@ -34,7 +34,7 @@ const DashboardHeader = () => {
 
         <span className='operator-hide' onClick={logoutVisiblity}><p className="logo-lg"> {(localStorage.getItem('user'))?.toUpperCase()}</p><img src={icon}></img></span>
         <div className='navbar-custom-menu'>
-          <ul className="dropdown-menu" style={{ display: dashbvisi ,position:'fixed',background:'white'}}>
+          <ul className="dropdown-menu" style={{ display: dashbvisi,background:'white'}}>
             <li className="user-header mx-1 my-1">
               <span className="flex flex-col items-center justify-center items-center"><img src={icon2} alt='Operator Icon' className="img-header"></img></span>
               <p className="text-logout">Welcome, {localStorage.getItem('user')}</p>
