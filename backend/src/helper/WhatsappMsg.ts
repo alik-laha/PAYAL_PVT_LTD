3dd007a100b8fbfc5d4a9b994e8998510fb1d49c
alik-laha <aliklaha@outlook.com>
2024-06-18 09:37:30 +0530

    wp number to send msg

--- backend/src/helper/WhatsappMsg.ts
+++ backend/src/helper/WhatsappMsg.ts
@@ -1,38 +1,46 @@
 import axios from 'axios'
 
+const Number = ["+918610808251", "+917362926736"]
+const tamplete = "hello_world"
 const WhatsappMsg = async (msg: string) => {
-    const data = {
-        messaging_product: "whatsapp",
-        to: "+918610808251",
-        type: "template",
-        template: {
-            name: "hello_world",
-            language: {
-                code: "en_US"
+
+    Number.map((num) => {
+        const data = {
+            messaging_product: "whatsapp",
+            to: num,
+            type: "template",
+            template: {
+                name: tamplete,
+                language: {
+                    code: "en_US"
+                }
+            }
+        };
+        try {
+            axios.post(process.env.WP_API_URL!, data, {
+                headers: {
+                    'Authorization': `Bearer ${process.env.WP_API_TOKEN}`,
+                    'Content-Type': 'application/json'
+                }
+            }).then((response) => {
+                console.log(response)
+                return response
+            }).catch((err) => {
+                console.log(err)
+                return err
+            })
+
+        }
+        catch {
+            (err: any) => {
+                console.log(err)
+                return err
             }
         }
-    };
 
-    try {
-        axios.post(process.env.WP_API_URL!, data, {
-            headers: {
-                'Authorization': `Bearer ${process.env.WP_API_TOKEN}`,
-                'Content-Type': 'application/json'
-            }
-        }).then((response) => {
-            console.log(response)
-            return response
-        }).catch((err) => {
-            console.log(err)
-            return err
-        })
+    })
 
-    }
-    catch {
-        (err: any) => {
-            console.log(err)
-            return err
-        }
-    }
+
+
 }
 export default WhatsappMsg
