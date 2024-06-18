d3124331dd043aa00ac1e2ca73cea467b0fa5857
alik-laha <aliklaha@outlook.com>
2024-06-18 09:17:13 +0530

    redirect to login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,51 +10,53 @@ import DashboardUser from './components/dashboard user/DashboardUser'
 import Machine from './components/Machine/Machine'
 import Private from './components/private/private'
 import QCRcn from './components/qcRCN/QCRcn'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
-
+  const navigate = useNavigate()
   return (
     <>
 
       <Routes>
+        <Route path='/' element={<>{navigate("/login")}</>} />
         <Route path="/login" element={<Login />} />
 
-        <Route element={<Private allowedRoles={['Director', 'FactoryManager', 
-        'ReceivingSupervisor', 'ReceivingManager',
-        'MaintainanceSupervisor',
-        'GradingSupervisor' , 'QCManager','ProductionManager','QCSupervisor']} />}>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<Private allowedRoles={['Director', 'FactoryManager',
+          'ReceivingSupervisor', 'ReceivingManager',
+          'MaintainanceSupervisor',
+          'GradingSupervisor', 'QCManager', 'ProductionManager', 'QCSupervisor']} />}>
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
 
         <Route element={<Private allowedRoles={['Director']} />}>
-        <Route path="/dashboard/user" element={<DashboardUser />} />
+          <Route path="/dashboard/user" element={<DashboardUser />} />
         </Route>
 
         <Route element={<Private allowedRoles={['Director', 'FactoryManager']} />}>
-        <Route path="/dashboard/employee" element={<Employee />} />
+          <Route path="/dashboard/employee" element={<Employee />} />
         </Route>
 
         <Route element={<Private allowedRoles={['Director', 'FactoryManager']} />}>
-        <Route path="/dashboard/machine" element={<Machine />} />
+          <Route path="/dashboard/machine" element={<Machine />} />
         </Route>
 
         <Route element={<Private allowedRoles={['Director', 'FactoryManager',
-        'ReceivingSupervisor', 'ReceivingManager']} />}>
-        <Route path="/dashboard/rcnprimaryentry" element={<RcnPrimaryEntry />} />
+          'ReceivingSupervisor', 'ReceivingManager']} />}>
+          <Route path="/dashboard/rcnprimaryentry" element={<RcnPrimaryEntry />} />
         </Route>
-        
+
         <Route element={<Private allowedRoles={['Director', 'FactoryManager',
-        'QCSupervisor', 'QCManager']} />}>
-        <Route path="/dashboard/qcRCN" element={<QCRcn />} />
+          'QCSupervisor', 'QCManager']} />}>
+          <Route path="/dashboard/qcRCN" element={<QCRcn />} />
         </Route>
-        
+
         <Route element={<Private allowedRoles={['Director', 'FactoryManager',
-        'GradingSupervisor', 'ProductionManager']} />}>
-        <Route path='/dashboard/rcnGrading' element={<RcnGrading />} />
+          'GradingSupervisor', 'ProductionManager']} />}>
+          <Route path='/dashboard/rcnGrading' element={<RcnGrading />} />
         </Route>
-       
-        
-       
+
+
+
       </Routes>
     </>
   )
