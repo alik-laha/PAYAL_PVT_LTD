f8b8b48fa851638e63bb31f03161a95526f7547c
indranil <indranilnonofficial007@gmail.com>
2024-07-01 03:49:40 +0530

    'passwordui'

--- backend/src/controller/forgotPassword/forgotPassword.ts
+++ backend/src/controller/forgotPassword/forgotPassword.ts
@@ -13,7 +13,11 @@ export default async function forgotPassword(req: Request, res: Response) {
         console.log(email)
         const employee: any = await Employee.findOne({ where: { email } });
         if (!employee) {
-            return res.status(404).json({ error: "No Employee Found With This Email ID" });
+            return res.status(404).json({ error: "No User Found with Email ID" });
+        }
+        const userData: any = await User.findOne({ where: { employeeId: employee.employeeId } })
+        if (!userData) {
+            return res.status(404).json({ error: "Employee is Not Registered as a User" });
         }
         let verificationCode = 0;
         crypto.randomInt(100000, 999999, (err, n) => {
@@ -22,23 +26,21 @@ export default async function forgotPassword(req: Request, res: Response) {
         });
 
         console.log(verificationCode)
-        const verificationCodeTime = Date.now() + 5 * 60 * 1000;
-        const userData: any = await User.findOne({ where: { employeeId: employee.employeeId } })
+        const verificationCodeTime = Date.now() + Number(process.env.ForgotpwdTokenExpireTime) * 60 * 1000;
+       
         const forgotData: any = await forgotPasswordModel.findOne({ where: { userId: userData.id } });
         if (forgotData) {
             await forgotData.destroy();
         }
         console.log(verificationCodeTime)
-        if (!userData) {
-            return res.status(404).json({ error: "Check the email this email is not Registered" });
-        }
+     
         const forgot = await forgotPasswordModel.create({ email, EmployeeId: employee.employeeId, verificationCode, verificationCodeTime, userId: userData.id });
         if (!forgot) {
             return res.status(500).json({ error: "Internal Server Error" });
         }
         const Mail = await ResetPassword(email, verificationCode);
         if (Mail) {
-            return res.status(200).json({ message: "Verification Code Sent to your Email" });
+            return res.status(200).json({ message: "Verification Code Has Been Sent to Email" });
         }
         return res.status(500).json({ error: "Internal Server Error" });
 
--- client/src/components/forgotPassword/EmailEntryforResetpassword.tsx
+++ client/src/components/forgotPassword/EmailEntryforResetpassword.tsx
@@ -1,23 +1,50 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Input } from "@/components/ui/input"
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import img from '../../assets/Static_Images/Company Logo.jpeg'
 import { Button } from "../ui/button";
+import tick from '../../assets/Static_Images/Flat_tick_icon.svg.png';
+
 
 const EmailEntryforResetpassword = () => {
     const emailRef = useRef<HTMLInputElement>(null);
     const navigate = useNavigate();
+    const [errView, setErrView] = useState<string>("none");
+    const [errMsg, setErrMsg] = useState<string>('');
+    const successdialog = document.getElementById('userscs') as HTMLInputElement;
+    const closeDialogButton = document.getElementById('userscsbtn') as HTMLInputElement;
+
+    if (closeDialogButton) {
+        closeDialogButton.addEventListener('click', () => {
+            if (successdialog != null) {
+                (successdialog as any).close();
+                navigate('/changePassword')
+                localStorage.setItem('autherized', 'true')
+            }
+        });
+    }
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const email = emailRef.current?.value;
         axios.post('/api/resetPassword/forgotPassword', { email }).then(res => {
             console.log(res.data)
-            navigate('/changePassword')
-            localStorage.setItem('autherized', 'true')
+            if (successdialog != null) {
+                (successdialog as any).showModal();
+            }
+            // navigate('/changePassword')
+            // localStorage.setItem('autherized', 'true')
         }).catch(err => {
             console.log(err)
+            if(err.response.data.error==='No User Found with Email ID'){
+                setErrView('block');
+                setErrMsg(err.response.data.error)
+            }
+            if(err.response.data.error==='Employee is Not Registered as a User'){
+                setErrView('block');
+                setErrMsg(err.response.data.error)
+            }
         })
     }
     return (
@@ -30,14 +57,20 @@ const EmailEntryforResetpassword = () => {
         <h3 className="text-sm font-sans mb-8 font-semibold pb-1 pt-2 text-cyan-700">Enter Email ID to Reset your Password</h3>
             <form  className="flex flex-col gap-4 w-64" onSubmit={handleSubmit}>
                 <Input type="email" placeholder="Email" ref={emailRef} />
-
+                <span style={{ display: errView }} className="text-red-600 text-sm font-sans font-semibold w-100 text-center">{errMsg}</span>
                 <Button className="bg-orange-500 mb-1 mt-7 mb-4" type="submit">Submit</Button>
                 
             </form>
             </div>
             </div>
 
-
+            <dialog id="userscs" className="dashboard-modal">
+                <button id="userscsbtn" className="dashboard-modal-close-btn">X</button>
+                <span className="flex">
+                    <img src={tick} height={2} width={35} alt='tick_image' />
+                    <p id="modal-text" className="pl-3 mt-1 font-medium">Verification Code has been sent to Email</p>
+                </span>
+            </dialog>
 
        
             
