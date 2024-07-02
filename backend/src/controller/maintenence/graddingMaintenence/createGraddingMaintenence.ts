e0c423c5dba6593b3ec444c2a83bb0d5960716c6
alik-laha <aliklaha@outlook.com>
2024-07-02 22:07:11 +0530

    backend started

---
+++ backend/src/controller/maintenence/graddingMaintenence/createGraddingMaintenence.ts
@@ -0,0 +1,7 @@
+import multer from "multer";
+import { Request, Response } from "express";
+
+const CreateBoillingMaintenence = async (req: Request, res: Response) => {
+
+}
+export default CreateBoillingMaintenence;
