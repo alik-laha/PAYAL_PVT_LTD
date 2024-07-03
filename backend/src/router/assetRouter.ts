e5a4a9c767e9dcb2899876b918e6e1cc403c2939
alik-laha <aliklaha@outlook.com>
2024-07-03 19:17:33 +0530

    view asset on the gradding search

--- backend/src/router/assetRouter.ts
+++ backend/src/router/assetRouter.ts
@@ -7,6 +7,7 @@ import UpdateAsset from '../controller/assetController/updateAsset';
 import deleteMachine from '../controller/assetController/deleteMachine';
 import getMechineByType from '../controller/assetController/getAllMachineBytype';
 import jwtVerify from '../middleWare/JwtAuthantication';
+import getAllActiveMachineForDropDown from '../controller/assetController/getallActiveMachine';
 
 const router = express.Router();
 
@@ -21,6 +22,8 @@ router.put('/assetupdate', jwtVerify, UpdateAsset)
 router.delete('/deleteAsset/:id', jwtVerify, deleteMachine)
 
 router.get('/getMachineByType/:type', jwtVerify, getMechineByType)
+
+router.get('/getallActiveMachine', jwtVerify, getAllActiveMachineForDropDown)
 
 
 export default router;
--- client/src/components/Cleaning/GraddingCleaning/GraddingCleaningTable.tsx
+++ client/src/components/Cleaning/GraddingCleaning/GraddingCleaningTable.tsx
@@ -1,28 +1,28 @@
 import { Input } from "../../ui/input";
 import { Button } from "@/components/ui/button";
 import { FaSearch } from "react-icons/fa";
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { AssetData } from "@/type/type";
 
 
 const GraddingMaintenanceTable = () => {
     const [fromdate, setfromDate] = React.useState<string>('');
     const [todate, settoDate] = React.useState<string>('');
     const [hidetodate, sethidetoDate] = React.useState<string>('');
+    const [allMachine, setAllMachine] = React.useState<AssetData[]>([]);
+    const [mc_name, setMc_name] = React.useState<string>('');
 
     const handleTodate = (e: React.ChangeEvent<HTMLInputElement>) => {
-
         const selected = e.target.value;
         if (!selected) {
             settoDate('')
             sethidetoDate('')
             return
         }
-        //console.log(selected)
         const date = new Date(selected)
         date.setDate(date.getDate() + 1);
-        //console.log(date)
         const nextday = date.toISOString().split('T')[0];
-        //console.log(nextday)
         sethidetoDate(selected)
         settoDate(nextday)
     }
@@ -30,21 +30,32 @@ const GraddingMaintenanceTable = () => {
     const handleSearch = () => {
         console.log(fromdate, todate)
     }
+    useEffect(() => {
+        axios.get('/api/asset/getallActiveMachine')
+            .then((res) => {
+                console.log(res)
+                setAllMachine(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    }, [])
     return (
         <div className="ml-5 mt-5 ">
             <div className="flex flexbox-search">
-                {/* <select className='flexbox-search-width flex h-8 w-1/5 ml-10 items-center justify-between rounded-md border border-input bg-background px-3 py-1 text-sm 
+                <select className='flexbox-search-width flex h-8 w-1/5 ml-10 items-center justify-between rounded-md border border-input bg-background px-3 py-1 text-sm 
     ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1'
-                    onChange={(e) => setOrigin(e.target.value)} value={origin}>
+                    onChange={(e) => setMc_name(e.target.value)} value={mc_name}>
                     <option className='relative flex w-full cursor-default select-none items-center rounded-sm 
         py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50' value=''>Origin (All)</option>
-                    {Origin.map((data, index) => (
+                    {allMachine.map((data, index) => (
                         <option className='relative flex w-full cursor-default select-none items-center rounded-sm 
-            py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50' value={data} key={index}>
-                            {data}
+            py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50' value={data.machineName} key={index}>
+                            {data.machineName}
                         </option>
                     ))}
-                </select> */}
+                </select>
 
                 <label className="font-semibold mt-1 ml-8 mr-5 flexbox-search-width-label-left ">From </label>
                 <Input className="w-1/6 flexbox-search-width-calender"
