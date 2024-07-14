6857ef93207fa3720b5b150f3d20308a245f4503
alik-laha <aliklaha@outlook.com>
2024-07-14 14:59:08 +0530

    boiler on the way

--- backend/src/model/cleaningBoilerModel.ts
+++ backend/src/model/cleaningBoilerModel.ts
@@ -1,7 +1,7 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/databaseConfig"
 
-const CleaningBoilling = sequelize.define('cleaningCooking', {
+const CleaningBoilling = sequelize.define('cleaningBoiler', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
@@ -15,37 +15,87 @@ const CleaningBoilling = sequelize.define('cleaningCooking', {
         type: DataTypes.DATE,
         allowNull: false,
     },
-    motorAndOtherPartsCleaning: {
+    rowWaterTank: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
     },
-    cookingInsideWashByStream: {
+    filterBackWash: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
     },
-    drainLineCleaning: {
+    softner: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
     },
-    waterWashChemberCleaning: {
+    softWaterStockTank_1st: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
     },
-    pressureGageCleanning: {
+    softWaterStockTank_2nd: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
     },
-    hopper: {
+    srainer_1: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
     },
-    elevetorCup: {
+    srainer_2: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    srainer_3: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    ecnomizerTube: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    nonReturnValve_1: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    nonReturnValve_2: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    nonReturnValve_3: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    tubeBrushing: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    waterGage: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    pessureGage: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    allBoillerArea: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    allBoillerPartsDust: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
