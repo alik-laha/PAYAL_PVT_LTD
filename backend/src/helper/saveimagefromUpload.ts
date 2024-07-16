f24a80baea2c5e0473db2fb6ef3ff335235166c3
alik-laha <aliklaha@outlook.com>
2024-07-17 01:09:43 +0530

    image of employee added

--- backend/src/helper/saveimagefromUpload.ts
+++ backend/src/helper/saveimagefromUpload.ts
@@ -1,7 +1,30 @@
 import multer from 'multer';
+import { NextFunction } from 'express';
 import path from 'path';
 
 let storage
+
+export const EmployeeImageUpload = (req: any, res: any, next: NextFunction) => {
+    const Employee = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, './uploads/employeeImages/');
+        },
+        filename: function (req, file, cb) {
+            const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+            cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
+        }
+    })
+    storage = Employee
+    const upload = multer({ storage, }).fields([
+        { name: 'employeeImage', maxCount: 10 },
+    ])
+    upload(req, res, (err) => {
+        if (err) {
+            return res.status(500).json({ msg: 'Error uploading file', error: err });
+        }
+        next();
+    });
+}
 
 export const graddingCleanFunction = () => {
     //graddingMaintenence clean image upload
--- backend/src/router/employRouter.ts
+++ backend/src/router/employRouter.ts
@@ -8,10 +8,11 @@ import DeleteEmployee from "../controller/employeeController/deleteEmployee";
 import releseEmployee from "../controller/employeeController/releseEmployee";
 import activeEmployeeCount from "../controller/employeeController/activeEmployeeCount";
 import jwtVerify from "../middleWare/JwtAuthantication";
+import { EmployeeImageUpload } from "../helper/saveimagefromUpload";
 
 const router = express.Router();
 
-router.post("/createemployee", employeeMiddleWare, jwtVerify, createEmployee)
+router.post("/createemployee", EmployeeImageUpload, employeeMiddleWare, jwtVerify, createEmployee);
 
 router.put("/updateemployee/:id", employeeMiddleWare, jwtVerify, updateEmployee)
 
