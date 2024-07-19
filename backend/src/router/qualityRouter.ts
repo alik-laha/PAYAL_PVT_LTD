d45d1dfc4fae30f4671444c11ab67d0be7470a72
alik-laha <aliklaha@outlook.com>
2024-07-19 09:44:12 +0530

    receving packaging metrial on the way

--- backend/src/router/qualityRouter.ts
+++ backend/src/router/qualityRouter.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import RecivingPackageMaterial from '../controller/Quality/RecivingPackageMaterial';
 import SkudataFind from '../controller/Quality/SkudataFind';
 import vendorNameFind from '../controller/Quality/vendorNameFind';
+import viewReceivingPackageMetrial from '../controller/Quality/viewReceivingPackageMetrial';
 const router = express.Router();
 
 
 router.post("/createrecivingpackagematerial", RecivingPackageMaterial)
 router.post("/skudatafind", SkudataFind)
 router.post("/vendornamefind", vendorNameFind)
+router.post("/getreceivematerial", viewReceivingPackageMetrial)
 
 export default router;
