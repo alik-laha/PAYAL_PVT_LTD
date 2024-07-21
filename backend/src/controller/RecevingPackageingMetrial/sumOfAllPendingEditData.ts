f1bcb866ecff9d590eeb4fc6bacfe83f8e396863
alik-laha <aliklaha@outlook.com>
2024-07-21 21:16:30 +0530

    receving edit reject done

--- backend/src/controller/RecevingPackageingMetrial/sumOfAllPendingEditData.ts
+++ backend/src/controller/RecevingPackageingMetrial/sumOfAllPendingEditData.ts
@@ -3,10 +3,36 @@ import EditPackagingMaterial from "../../model/editPackageingMaterialModel";
 import PackagingMaterial from "../../model/recevingPackagingMaterialModel";
 import VendorName from "../../model/vendorNameModel";
 import SkuData from "../../model/SkuModel";
+import { Op } from "sequelize";
 
 const sumOfAllRecenvingPackageMaterial = async (req: Request, res: Response) => {
     try {
-        const sumOfAllRecenvingPackageMaterial = await EditPackagingMaterial.count();
+        const today = new Date();
+        let Year = today.getFullYear()
+
+        const compareDate = new Date(`${Year}-04-01`);
+        compareDate.setHours(0, 0, 0, 0)
+        let targetDate
+        if (today < compareDate) {
+            targetDate = new Date(`${Year - 1}-04-01`);
+        }
+        else {
+            targetDate = new Date(`${Year}-04-01`);
+        }
+
+
+        targetDate.setHours(0, 0, 0, 0)
+        if (today.getHours() < 5 || (today.getHours() === 5 && today.getMinutes() <= 30)) {
+            today.setHours(today.getHours() + 5);
+            today.setMinutes(today.getMinutes() + 30);
+        }
+        const sumOfAllRecenvingPackageMaterial = await EditPackagingMaterial.count({
+            where: {
+                date: {
+                    [Op.between]: [targetDate, today]
+                }
+            }
+        });
         const vendorName = await VendorName.count();
         const skuData = await SkuData.count();
         const packagingMaterial = await PackagingMaterial.count();
