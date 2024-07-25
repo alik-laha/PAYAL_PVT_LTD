0819db4261617498912c3e1fa1f77a429bce2334
alik-laha <aliklaha@outlook.com>
2024-07-25 10:47:07 +0530

    model of quality packaging

--- backend/src/helper/saveimagefromUpload.ts
+++ backend/src/helper/saveimagefromUpload.ts
@@ -1,31 +1,32 @@
 import multer from 'multer';
-import { NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import path from 'path';
 
 let storage
 
-export const EmployeeImageUpload = (req: any, res: any, next: NextFunction) => {
-    const Employee = multer.diskStorage({
-        destination: function (req, file, cb) {
+export const EmployeeImageUpload = (req: Request, res: Response, next: NextFunction) => {
+    console.log('EmployeeImageUpload')
+    const storage = multer.diskStorage({
+        destination: (req, file, cb) => {
             cb(null, './uploads/employeeImages/');
         },
-        filename: function (req, file, cb) {
+        filename: (req, file, cb) => {
             const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
             cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
         }
-    })
-    storage = Employee
-    console.log(req)
-    const upload = multer({ storage, }).fields([
-        { name: 'employeeImage', maxCount: 10 },
-    ])
+    });
+
+    const upload = multer({ storage }).fields([
+        { name: 'employeeImage', maxCount: 1 },
+    ]);
+
     upload(req, res, (err) => {
         if (err) {
             return res.status(500).json({ msg: 'Error uploading file', error: err });
         }
         next();
     });
-}
+};
 
 export const graddingCleanFunction = () => {
     //graddingMaintenence clean image upload
