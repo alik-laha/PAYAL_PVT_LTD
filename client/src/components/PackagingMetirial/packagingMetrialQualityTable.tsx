38a2a8c0b252987c5c843c51907fa2aaf538904b
alik-laha <aliklaha@outlook.com>
2024-07-27 00:10:00 +0530

    quality packaging on the way

---
+++ client/src/components/PackagingMetirial/packagingMetrialQualityTable.tsx
@@ -0,0 +1,11 @@
+
+
+
+const PackagingMetirialQualityTable = () => {
+    return (
+        <div>
+            <h1>Pac met Quality</h1>
+        </div>
+    )
+}
+export default PackagingMetirialQualityTable
