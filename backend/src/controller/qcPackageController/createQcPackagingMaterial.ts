d20f5f351ac23c39500f18ede2d15a60078a4f5b
alik-laha <aliklaha@outlook.com>
2024-07-27 08:44:21 +0530

    quality packaging on the way

---
+++ backend/src/controller/qcPackageController/createQcPackagingMaterial.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+
+const CreateQcPackagingMaterial = async (req: Request, res: Response) => {
+    try {
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+export default CreateQcPackagingMaterial;
