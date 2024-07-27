71da85a5f8b93c92273316fbdb590fb13fd6c87b
alik-laha <aliklaha@outlook.com>
2024-07-27 20:33:39 +0530

    quality packaging on the way

--- backend/src/middleWare/QualityPackageMetrialMiddleWare.ts
+++ backend/src/middleWare/QualityPackageMetrialMiddleWare.ts
@@ -11,17 +11,28 @@ const QualityPackageMetrialMiddleWare = (req: Request, res: Response, next: Next
             },
             filename: (req, file, cb) => {
                 if (file.fieldname === 'foodGradeCirtiFicateFile') {
-                    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-                    cb(null, `foodGradeCirtiFicateFile-${uniqueSuffix}${path.extname(file.originalname)}`);
+                    if (!file.originalname.match(/\.(pdf)$/)) {
+                        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+                        cb(null, `foodGradeCirtiFicateFile-${uniqueSuffix}${path.extname(file.originalname)}`);
+                    }
                 }
-
-                const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-                cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
+                if (file.fieldname === 'coaCirtificateFile') {
+                    if (!file.originalname.match(/\.(pdf)$/)) {
+                        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+                        cb(null, `coaCirtificateFile-${uniqueSuffix}${path.extname(file.originalname)}`);
+                    }
+                }
+                if (file.fieldname === 'damageFile') {
+                    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+                    cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
+                }
             }
         });
 
         const upload = multer({ storage }).fields([
             { name: 'foodGradeCirtiFicateFile', maxCount: 1 },
+            { name: 'coaCirtificateFile', maxCount: 1 },
+            { name: 'damageFile', maxCount: 10 }
         ]);
 
         upload(req, res, (err) => {
