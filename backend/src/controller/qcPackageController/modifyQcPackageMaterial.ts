f7702fdf9e4f0ed839b689be84dcf65b5063eaa8
alik-laha <aliklaha@outlook.com>
2024-07-29 20:57:06 +0530

    rcn scooping edit done

--- backend/src/controller/qcPackageController/modifyQcPackageMaterial.ts
+++ backend/src/controller/qcPackageController/modifyQcPackageMaterial.ts
@@ -6,6 +6,7 @@ const ModifyQcPackageMaterial = async (req: Request, res: Response) => {
     try {
         const { testingDate, length, width, height, gsm, avgWeight, leakageTest, dropTest, sealCondition, labelingCondition, coa, foodGradeCirtiicate, remarks } = req.body;
         const name = req.cookies.user;
+        const id = req.params.id;
         const files: any = req.files;
         let foodGradeCirtiFicateFile: string = "";
         let coaCirtificateFile: string = "";
@@ -19,7 +20,32 @@ const ModifyQcPackageMaterial = async (req: Request, res: Response) => {
         if (files.damagePartsImage) {
             files.damagePartsImage.map((file: any) => damagePartsImage.push(file.path));
         }
-
+        const qualityPackageMaterial: any = await QualityPackageMaterial.findOne({ where: { id } });
+        if (!qualityPackageMaterial) {
+            return res.status(404).json({ error: "Quality Package Material not found" });
+        }
+        await QualityEditPackageMaterial.create({
+            testingDate: qualityPackageMaterial.testingDate,
+            length: qualityPackageMaterial.length,
+            width: qualityPackageMaterial.width,
+            height: qualityPackageMaterial.height,
+            gsm: qualityPackageMaterial.gsm,
+            avgWeight: qualityPackageMaterial.avgWeight,
+            leakageTest: qualityPackageMaterial.leakageTest,
+            dropTest: qualityPackageMaterial.dropTest,
+            sealCondition: qualityPackageMaterial.sealCondition,
+            labelingCondition: qualityPackageMaterial.labelingCondition,
+            coa: qualityPackageMaterial.coa,
+            foodGradeCirtiicate: qualityPackageMaterial.foodGradeCirtiicate,
+            remarks: qualityPackageMaterial.remarks,
+            foodGradeCirtificateStatus: qualityPackageMaterial.foodGradeCirtificateStatus,
+            foodGradeCirtiFicateFile: qualityPackageMaterial.foodGradeCirtiFicateFile,
+            coaCirtificateStatus: qualityPackageMaterial.coaCirtificateStatus,
+            coaCirtificateFile: qualityPackageMaterial.coaCirtificateFile,
+            damageFile: qualityPackageMaterial.damageFile,
+            qualityPackageMaterialId: qualityPackageMaterial.id,
+            createdBy: qualityPackageMaterial.createdBy,
+        });
 
     }
     catch (err) {
