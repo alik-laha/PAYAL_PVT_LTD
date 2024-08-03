ddb09fa1aae5f7fa7fedc05f545afbf5655a7553
indranil <indranilnonofficial007@gmail.com>
2024-08-03 22:48:37 +0530

    uichange

--- client/src/components/gatepass In/GatePassIn.tsx
+++ client/src/components/gatepass In/GatePassIn.tsx
@@ -14,60 +14,22 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
-
-import Context from '../context/context';
-import { useContext, useState } from 'react';
-
-import axios from 'axios'
-import UseQueryData from '../common/dataFetcher';
-import Loader from '../common/Loader';
-import { BormapendingLotData } from '@/type/type';
 import GatePassCreateForm from './gatepasscreateform';
+
+
 
 
 
 const GatepassIn = () => {
 
-    const { setEditBormaLotWiseData } = useContext(Context)
-    const [lotdata, setLotData] = useState<BormapendingLotData[]>([])
 
 
 
-    const { data, isLoading, error } = UseQueryData('/api/borma/sumofallborma', 'GET', 'AllBormaSum');
-    const handleEditFetch = async () => {
-
-        axios.get("/api/scooping/findEditBormaAll").then(res => {
-            console.log(res)
-            setEditBormaLotWiseData(res.data.scoopingAllEdit)
-        })
-            .catch(err => {
-                console.log(err)
-            })
-
-    }
-    if (isLoading) {
-        return <Loader />
-    }
-
-    if (error) {
-        return <div>Error</div>;
-    }
-    console.log(data)
-
-    const handleOpenLotNo = async () => {
-        axios.get('/api/borma/getUnBormaEntry/0').then(res => {
-            console.log(res)
-            setLotData(res.data.scoopingLot)
-        })
+ 
+ 
 
 
-
-
-    }
-
-    function formatNumber(num: any) {
-        return Number.isInteger(num) ? parseInt(num) : num.toFixed(2);
-    }
+ 
     return (
         <div>
             <DashboardHeader />
@@ -75,15 +37,15 @@ const GatepassIn = () => {
             <div className='dashboard-main-container'>
                 <div className="flexbox-header">
                     <div className="flexbox-tile bg-red-500 hover:bg-orange-400">
-                        IN PASS ISSUED <br /><p>{data.data[0].India ? formatNumber(data.data[0].India)  : 5} </p>
+                        IN PASS ISSUED <br /><p>5 </p>
                     </div>
                     
                     <div className="flexbox-tile bg-green-500 hover:bg-orange-400">
-                       APPROVED <br /><p>{data.data[0].Ghana? formatNumber(data.data[0].Ghana) : 3} </p>
+                       APPROVED <br /><p>3 </p>
                     </div>
                    
                     <div className="flexbox-tile bg-purple-500 hover:bg-orange-400">
-                        PENDING APPROVAL <br /><p>{data.data[0].Tanzania ? formatNumber(data.data[0].Tanzania) : 2} </p>
+                        PENDING APPROVAL <br /><p>2 </p>
                     </div>
                    
 
@@ -96,7 +58,7 @@ const GatepassIn = () => {
 
                 <div>
                     <Dialog>
-                        <DialogTrigger> <Button className="bg-red-500 mb-2 mt-5 ml-4" onClick={handleOpenLotNo}>+ Add New Entry</Button></DialogTrigger>
+                        <DialogTrigger> <Button className="bg-red-500 mb-2 mt-5 ml-4">+ Add New Entry</Button></DialogTrigger>
                         <DialogContent className='max-w-2xl'>
                             <DialogHeader>
                                 <DialogTitle><p className='text-1xl pb-1 text-center mt-2'>GatePass Entry Form</p></DialogTitle>
@@ -108,7 +70,7 @@ const GatepassIn = () => {
                     </Dialog>
 
 
-                    <Button className="bg-orange-400 mb-2 ml-8 responsive-button-adjust" onClick={handleEditFetch}> Pending Edit ({data.EditData})</Button> 
+                    <Button className="bg-orange-400 mb-2 ml-8 responsive-button-adjust" > Pending Edit ()</Button> 
 
                 </div>
                 
