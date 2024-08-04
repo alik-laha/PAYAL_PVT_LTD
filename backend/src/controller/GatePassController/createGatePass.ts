3721d9c182861a4426f3f7b720ac2fd23454297a
indranil <indranilnonofficial007@gmail.com>
2024-08-04 21:06:36 +0530

    gatepassmodel

--- backend/src/controller/GatePassController/createGatePass.ts
+++ backend/src/controller/GatePassController/createGatePass.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from "express";
+import gatePass from "../../model/gatepassModel";
 
-import LotNo from "../../model/lotNomodel";
-import { lotNoData } from "../../type/type";
 
-const CreateLotNo = async (req: Request, res: Response) => {
-try{}
+
+const CreateGatePass = async (req: Request, res: Response) => {
+try{
+
+    const feeledBy = req.cookies.user;
+    const lastgatepass = await gatePass.findOne({ order: [['id', 'DESC']] }) ;
+    const newId = lastgatepass ? lastgatepass.dataValues.id + 1 : 1;
+    const gatepassNo = `PDPL${String(newId).padStart(5, '0')}`;
+    await gatePass.create({ gatePassNo: gatepassNo ,createdBy:feeledBy})
+    //console.log('New sequence generated and saved:', newSequence);
+    return res.status(201).json({ msg: `New GatePass ${gatepassNo} is Created`, gatepassNo });
+}
+catch (err) {
+    return res.status(500).json({ message: "Duplicate Lot No. or Error in Creating GatePass", err });
+}
+
+}
+export default CreateGatePass;
 
--- backend/src/router/gatePassRouter.ts
+++ backend/src/router/gatePassRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
 import jwtVerify from '../middleWare/JwtAuthantication';
+import CreateGatePass from '../controller/GatePassController/createGatePass';
 const router = express.Router();
 
-//router.post("/createGatePass", jwtVerify, getEmployeeDetail)
+router.post("/createGatePass", jwtVerify, CreateGatePass)
 
 
 
