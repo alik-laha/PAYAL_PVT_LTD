8b1f10f5e5dac82e15465b75b7ff04827082a7b0
indranil <indranilnonofficial007@gmail.com>
2024-08-12 01:40:28 +0530

    approvegatepass

--- backend/src/helper/WpMsgGatePassRcv.ts
+++ backend/src/helper/WpMsgGatePassRcv.ts
@@ -8,7 +8,7 @@ const WP_NUMBER_GATEPASS_MANAGER = process.env.WP_NUMBER_GATEPASS_MANAGER ? proc
 const WpMsgGatePassRcv = async (tablename: string, gatepassNo: string, template: string,section:string) => {
 
     if (template === 'gatepass_rcv_dispatch') {
-        if(section==='RCN Cashew IN'){
+        if (section === 'RCN Cashew IN') {
             Number_IN_RCN.map((num) => {
                 const data = {
                     messaging_product: "whatsapp",
@@ -49,7 +49,7 @@ const WpMsgGatePassRcv = async (tablename: string, gatepassNo: string, template:
                         console.log(err)
                         return err
                     })
-    
+
                 }
                 catch {
                     (err: any) => {
@@ -57,10 +57,10 @@ const WpMsgGatePassRcv = async (tablename: string, gatepassNo: string, template:
                         return err
                     }
                 }
-    
+
             })
         }
-        if(section==='PC IN'){
+        if (section === 'PC IN') {
             Number_IN_PC.map((num) => {
                 const data = {
                     messaging_product: "whatsapp",
@@ -101,7 +101,7 @@ const WpMsgGatePassRcv = async (tablename: string, gatepassNo: string, template:
                         console.log(err)
                         return err
                     })
-    
+
                 }
                 catch {
                     (err: any) => {
@@ -109,69 +109,69 @@ const WpMsgGatePassRcv = async (tablename: string, gatepassNo: string, template:
                         return err
                     }
                 }
-    
+
             })
         }
 
 
     }
-    if (template === 'rcv_dispatch_complt' || template === 'gatepass_release') {
-        
-            WP_NUMBER_SECURITY.map((num) => {
-                const data = {
-                    messaging_product: "whatsapp",
-                    to: num,
-                    type: "template",
-                    template: {
-                        name: template,
-                        language: {
-                            code: "en"
-                        },
-                        components: [
-                            {
-                                type: "body",
-                                parameters: [
-                                    {
-                                        type: "text",
-                                        text: tablename // This replaces {{1}}
-                                    },
-                                    {
-                                        type: "text",
-                                        text: gatepassNo // This replaces {{2}}
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                }
-                try {
-                    axios.post(process.env.WP_API_URL!, data, {
-                        headers: {
-                            'Authorization': `Bearer ${process.env.WP_API_TOKEN}`,
-                            'Content-Type': 'application/json'
+    if (template === 'rcv_dispatch_complt' ) {
+
+        WP_NUMBER_SECURITY.map((num) => {
+            const data = {
+                messaging_product: "whatsapp",
+                to: num,
+                type: "template",
+                template: {
+                    name: template,
+                    language: {
+                        code: "en"
+                    },
+                    components: [
+                        {
+                            type: "body",
+                            parameters: [
+                                {
+                                    type: "text",
+                                    text: tablename // This replaces {{1}}
+                                },
+                                {
+                                    type: "text",
+                                    text: gatepassNo // This replaces {{2}}
+                                }
+                            ]
                         }
-                    }).then((response) => {
-                        console.log(response)
-                        return response
-                    }).catch((err) => {
-                        console.log(err)
-                        return err
-                    })
-    
+                    ]
                 }
-                catch {
-                    (err: any) => {
-                        console.log(err)
-                        return err
+            }
+            try {
+                axios.post(process.env.WP_API_URL!, data, {
+                    headers: {
+                        'Authorization': `Bearer ${process.env.WP_API_TOKEN}`,
+                        'Content-Type': 'application/json'
                     }
+                }).then((response) => {
+                    console.log(response)
+                    return response
+                }).catch((err) => {
+                    console.log(err)
+                    return err
+                })
+
+            }
+            catch {
+                (err: any) => {
+                    console.log(err)
+                    return err
                 }
-    
-            })
-        
+            }
+
+        })
+
     }
 
     if (template === 'verify_gatepass') {
-        
+
         WP_NUMBER_GATEPASS_MANAGER.map((num) => {
             const data = {
                 messaging_product: "whatsapp",
@@ -222,8 +222,62 @@ const WpMsgGatePassRcv = async (tablename: string, gatepassNo: string, template:
             }
 
         })
-    
-}
+
+    }
+    if ( template === 'gatepass_release') {
+
+        WP_NUMBER_SECURITY.map((num) => {
+            const data = {
+                messaging_product: "whatsapp",
+                to: num,
+                type: "template",
+                template: {
+                    name: template,
+                    language: {
+                        code: "en"
+                    },
+                    components: [
+                        {
+                            type: "body",
+                            parameters: [
+                                {
+                                    type: "text",
+                                    text: gatepassNo // This replaces {{1}}
+                                },
+                                {
+                                    type: "text",
+                                    text: tablename // This replaces {{2}}
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+            try {
+                axios.post(process.env.WP_API_URL!, data, {
+                    headers: {
+                        'Authorization': `Bearer ${process.env.WP_API_TOKEN}`,
+                        'Content-Type': 'application/json'
+                    }
+                }).then((response) => {
+                    console.log(response)
+                    return response
+                }).catch((err) => {
+                    console.log(err)
+                    return err
+                })
+
+            }
+            catch {
+                (err: any) => {
+                    console.log(err)
+                    return err
+                }
+            }
+
+        })
+
+    }
 
 
 
