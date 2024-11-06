eb8bea4257ac07fa9e3329331e3e7b16f89f9510
indranil <indranilnonofficial007@gmail.com>
2024-11-06 14:51:05 +0530

    agarbati_final_netlify_fix

--- backend/src/controller/IssueController/searchIssueItem.ts
+++ backend/src/controller/IssueController/searchIssueItem.ts
@@ -2,24 +2,36 @@ import { Request, Response } from "express";
 
 import { Op } from "sequelize";
 
-import RcnAllScooping from "../../model/scoopingAllmodel";
-import RcnScooping from "../../model/scoopingModel";
 import ItemIssue from "../../model/itemissueModel";
+import sequelize from "../../config/databaseConfig";
 
 const SearchIssueItem = async (req: Request, res: Response) => {
     try {
         const page = parseInt(req.query.page as string, 10) || 0;
         const size = parseInt(req.query.limit as string, 10) || 0;
-        const { isssueId, fromDate, toDate, unit, section,type } = req.body;
+        const { isssueId, unit, section,type,fromDate, toDate } = req.body;
+       
         const offset = (page - 1) * size;
         const limit = size;
         let whereClause = []
+        let whereClause2 = []
         if (fromDate && toDate) {
-            whereClause.push({
-                date: {
-                    [Op.between]: [fromDate, toDate]
-                }
-            });
+            if(type == 'ItemWise'){
+                whereClause.push({
+                    date: {
+                        [Op.between]: [fromDate, toDate]
+                    }
+                });
+            }
+            else{
+                whereClause2.push({
+                    date: {
+                        [Op.between]: [fromDate, toDate]
+                    }
+                });
+            }
+           
+            
         }
         if (isssueId) {
             if (type == 'ItemWise') {
@@ -32,17 +44,42 @@ const SearchIssueItem = async (req: Request, res: Response) => {
             }
         }
         if (unit) {
-            whereClause.push({
-                sectionunit:unit
-            })
+            if (type == 'ItemWise') {
+                whereClause.push({
+                    sectionunit:unit
+                })
+            }
+            else{
+                whereClause2.push({
+                    sectionunit:unit
+                })
+            }
+           
         }
         if (section) {
-            whereClause.push({
-                section:section
+            if (type == 'ItemWise') {
+                whereClause.push({
+                    section:section
+                })
+            }
+            else{
+                whereClause2.push({
+                    section:section
+                })
+            }
+           
+        }
+        if (type == 'ItemWise') {
+            whereClause2.push({
+                [Op.or]: [
+                    { editStatus: 'Approved' },
+                    { editStatus: 'NA' }
+                ]
             })
         }
    
         const where = whereClause.length > 0 ? { [Op.and]: whereClause } : {};
+        const where2 = whereClause2.length > 0 ? { [Op.and]: whereClause2} : {};
         let GradingEntries;
         if (limit === 0 && offset === 0) {
 
@@ -54,14 +91,18 @@ const SearchIssueItem = async (req: Request, res: Response) => {
                 });
 
             } 
-            // else {
-            //     GradingEntries = await RcnScooping.findAll({
-            //         where,
-            //         order: [['LotNo', 'DESC'], ['Scooping_Line_Mc','ASC'],['date', 'DESC']] // Order by date descending
+            else {
+               GradingEntries = await ItemIssue.findAll({
+                    attributes: [
+                        'sectionunit',
+                        [sequelize.fn('sum', sequelize.col('totalPrice')), 'totalIssuePrice']
+                        
+                    ],
+                    where:where2,
+                    group: ['date','sectionunit','category']
+                });
 
-            //     });
-
-            // }
+            }
 
         }
         else {
@@ -73,15 +114,20 @@ const SearchIssueItem = async (req: Request, res: Response) => {
                     offset
                 });
             }
-            // else {
-            //     GradingEntries = await RcnScooping.findAll({
-            //         where,
-            //         order: [['LotNo', 'DESC'], ['Scooping_Line_Mc','ASC'],['date', 'DESC']],
-            //         limit,
-            //         offset
-            //     });
+            else {
+                
+                GradingEntries = await ItemIssue.findAll({
+                    attributes: [
+                        'date','sectionunit','category',
+                        [sequelize.fn('sum', sequelize.col('totalPrice')), 'totalIssuePrice']
+                        
+                    ],
+                    where: where2,
+                    group: ['date','sectionunit','category'],limit,
+                    offset
+                });
 
-            // }
+            }
 
         }
         return res.status(200).json(GradingEntries);
