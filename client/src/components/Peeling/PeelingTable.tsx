55408a68f1799906fd2de107fca2e00f4ede54ac
indranil <indranilnonofficial007@gmail.com>
2025-03-03 01:51:06 +0530

    wholesbasic

--- client/src/components/Peeling/PeelingTable.tsx
+++ client/src/components/Peeling/PeelingTable.tsx
@@ -320,13 +320,7 @@ const PeelingTable = () => {
         }
     }
 
-    const formatNumberWithSign = (number: number) => {
-        if (number > 0) {
-            return `+${number}`;
-        } else {
-            return `${number}`;
-        }
-    };
+  
     return (
         <>
 
--- client/src/components/Wholes/WholesTable.tsx
+++ client/src/components/Wholes/WholesTable.tsx
@@ -55,7 +55,7 @@ import { FcApprove, FcDisapprove } from "react-icons/fc";
 import { saveAs } from 'file-saver';
 import * as XLSX from 'xlsx';
 import RCNWholesReMix from "./WholesMix";
-import RCNWholesReCreateForm from "./WholesRecreateForm";
+// import RCNWholesReCreateForm from "./WholesRecreateForm";
 import WholesEditForm from "./WholesModify";
 
 
@@ -1330,7 +1330,7 @@ py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foregrou
                                                                     <p className='text-1xl pb-1 text-center mt-1'>Wholes Entry Reissue</p>
                                                                 </DialogTitle>
                                                             </DialogHeader>
-                                                            <RCNWholesReCreateForm borma={[item]} />
+                                                            {/* <RCNWholesReCreateForm borma={[item]} /> */}
                                                         </DialogContent>
 
                                                     </Dialog>}
