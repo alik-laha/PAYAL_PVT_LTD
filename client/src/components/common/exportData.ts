de9a6a713eb6f525eb662e5ae81f19ca6d454fa9
indranil <indranilnonofficial007@gmail.com>
2025-04-01 08:33:45 +0530

    'package'

--- client/src/components/common/exportData.ts
+++ client/src/components/common/exportData.ts
@@ -124,11 +124,11 @@ export const Size = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 export const QC_Boiler = ['Boiler-1 (Old)','Boiler-2 (New)']
 export const Session_LogoutTime_Hr = 12
 export const cookingTime = ['00:07', '00:08', '00:09', '00:10', '00:11', '00:12']
-export const Origin = ["India", "Ghana", "IVC", "Benin", "Tanzania", "Nigeria", "Togo", "Guinea-Bissau", "Senegal","GUINEA-CONAKRY"]
+export const Origin = ["India", "Ghana", "IVC", "Benin", "Tanzania", "Nigeria", "Togo", "Guinea-Bissau", "Senegal","Guinea-Conkary"]
 export const SelectType = ["LineWise","LotWise"]
 export const SelectTypeIssue = ["ItemWise","DayWise"]
 export const prodStockSection=['Wholes','LW','Sorting','DPDS','BigTaiho','Rejection']
-export const FY=['2024-25']
+export const FY=['2025-26']
 
 export const sectionDataonTypeGate = {
     IN: ["RawCashew", "PackagingMaterial","Store",'General','Almond','Village','Agarbati'],
