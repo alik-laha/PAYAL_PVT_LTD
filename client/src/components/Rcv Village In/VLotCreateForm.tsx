603f03687048f154ac96e81fcd4756dfc2ee4fcd
indranil <indranilnonofficial007@gmail.com>
2025-04-20 00:05:15 +0530

    'vlot1'

--- client/src/components/Rcv Village In/VLotCreateForm.tsx
+++ client/src/components/Rcv Village In/VLotCreateForm.tsx
@@ -9,12 +9,12 @@ interface VLOTRowData{
     id: number;
     origin: string;
     Receiving_Qty: string;
-    actual_Receiving_Qty: string;
+    actual_Receiving_Qty: number;
     type:string;
     sku:string;
     Vendor:string;
-    Loss:string;
-    Loss_prcntg:string;
+    Loss:number;
+    Loss_prcntg:number;
 }
 import {
     Table,
@@ -28,6 +28,7 @@ import tick from '../../assets/Static_Images/Flat_tick_icon.svg.png'
 import cross from '../../assets/Static_Images/error_img.png'
 import { useEffect, useState } from "react";
 import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 
 const VLOTCreateForm = (props:Props) => {
@@ -46,8 +47,9 @@ const VLOTCreateForm = (props:Props) => {
                 Vendor:item.vendorName,
                 Receiving_Qty: item.totalWt,
                 id:item.id,
-                actual_Receiving_Qty:item.totalWt,
-                
+                actual_Receiving_Qty:Number(item.totalWt),
+                Loss:0,
+                Loss_prcntg:0
             }));
           
             //console.log(initialform)
@@ -89,15 +91,16 @@ const VLOTCreateForm = (props:Props) => {
     const handleRowChangeActual = (index:number,e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault()
         handleRowChange(index,'actual_Receiving_Qty',e.target.value)
-        rows[index].huskprcntg=Number(((Number(e.target.value)/(rows[index].totalWt))*100).toFixed(2))
-        handleRowChange(index,'husk',e.target.value)
-       
+        console.log(rows[index].Loss)
+        rows[index].Loss= Number(rows[index].Receiving_Qty)-Number(rows[index].actual_Receiving_Qty)
+        rows[index].Loss_prcntg=((Number(rows[index].Receiving_Qty)-Number(e.target.value))/Number(rows[index].Receiving_Qty))*100
+        handleRowChange(index,'actual_Receiving_Qty',e.target.value)
      }  
 
     return(
         <>
-        <div className="px-5 py-2 overflow-auto max-h-screen">
-        <form className='flex flex-col gap-1 pt-1' >
+        <div className="px-5 py-2 mt-2 overflow-auto max-h-screen">
+        <form className='flex flex-col gap-1 pt-3' >
             <div className="overflow-auto max-h-screen">
                 <Table className="w-full mt-3">
                     <TableHeader className="bg-neutral-100 text-stone-950 ">
@@ -106,29 +109,49 @@ const VLOTCreateForm = (props:Props) => {
                             <TableHead className="text-center">Origin</TableHead>
                             <TableHead className="text-center">Item_Type</TableHead>
                             <TableHead className="text-center">Item_Name</TableHead>
-                            <TableHead className="text-center">Vendor</TableHead>
+                            <TableHead className="text-center">Receiving_Vendor</TableHead>
                             <TableHead className="text-center">Receiving Qty</TableHead>
                             <TableHead className="text-center">Actual_Qty</TableHead>
-                            <TableHead className="text-center">Loss</TableHead>
-                            <TableHead className="text-center">Loss_prcntg</TableHead>
+                            <TableHead className="text-center">Weight_Loss</TableHead>
+                            {/* <TableHead className="text-center">Loss_prcntg</TableHead> */}
                         </TableRow>
                     </TableHeader>
                     <TableBody>
                         {props.props.length > 0 ? (rows.map((row,idx:number) => (
-                            <TableRow key={row.id} >
+                            <TableRow key={row.id}  className="boiling-row-height-scoop">
                                  <TableCell className="text-center">{idx+1}</TableCell>
-                                <TableCell className="text-center font-semibold">{row.origin}</TableCell>
+                                <TableCell className="text-center font-semibold text-red-500">{row.origin}</TableCell>
                                 <TableCell className="text-center">{row.type}</TableCell>
                                 <TableCell className="text-center">{row.sku}</TableCell>
                                 <TableCell className="text-center">{row.Vendor}</TableCell>
-                                <TableCell className="text-center font-semibold text-red-500">{formatNumber(row.Receiving_Qty)} Kg</TableCell>
-                                <TableCell className="text-center"> <Input  value={row.actual_Receiving_Qty} placeholder="Qty" 
+                                <TableCell className="text-center font-semibold">{formatNumber(row.Receiving_Qty)} Kg</TableCell>
+                                <TableCell className="text-center"> <Input type="number" value={row.actual_Receiving_Qty} placeholder="Qty" 
                                 onChange={(e) => handleRowChangeActual(idx,e)} required /></TableCell>
-                           
+                            <TableCell className="text-center font-semibold text-blue-500">{row.Loss} Kg</TableCell>
+                            {/* <TableCell className="text-center font-semibold text-red-500">{row.Loss_prcntg.toFixed(2)} %</TableCell> */}
                             </TableRow>
                         ))):null}
                     </TableBody>
                 </Table>
+                <Button className="bg-orange-500  text-center items-center justify-center h-8 w-20" disabled={isdisable}>{isdisable? 'Submitting':'Submit'}</Button>
+                                
+                  <dialog id="successemployeedialog" className="dashboard-modal">
+                  <button id="empcloseDialog" className="dashboard-modal-close-btn ">X </button>
+                  <span className="flex"><img src={tick} height={2} width={35} alt='tick_image' />
+                      <p id="modal-text" className="pl-3 mt-1 font-medium">{errortext}</p>
+                  </span>
+  
+  
+              </dialog>
+  
+              <dialog id="erroremployeedialog" className="dashboard-modal">
+                  <button id="errorempcloseDialog" className="dashboard-modal-close-btn ">X </button>
+                  <span className="flex"><img src={cross} height={25} width={25} alt='error_image' />
+                      <p id="modal-text" className="pl-3 mt-1 text-base font-medium">{errortext}</p>
+                  </span>
+  
+  
+              </dialog>
             </div>
             </form>
         </div>
--- client/src/components/Rcv Village In/VlotInitial.tsx
+++ client/src/components/Rcv Village In/VlotInitial.tsx
@@ -95,7 +95,7 @@ const VLOTInitial = (props: any) => {
                                                     <Button className="bg-green-500 h-8 rounded-md" onClick={()=>handleLineEntry(item.recevingDate)} >+ VLOT </Button></DialogTrigger>
                                              <DialogContent style={{display:'block'}} className='max-w-6xl'>
                                                     <DialogHeader >
-                                                        <DialogTitle><p className='text-1xl text-center mt-1'>Village VLOT Entry</p></DialogTitle>
+                                                        <DialogTitle><p className='text-1xl text-center mt-1'> VLOT Entry</p></DialogTitle>
 
                                                     </DialogHeader>
                                                 <VLOTCreateForm props={rcnData}/>
